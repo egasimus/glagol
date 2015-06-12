565b58f53e9ad8a87fdc7f335f81be52e9f1943d
Adam Avramov <fallenblood@gmail.com>
2015-06-12 16:47:57 +0300

    something exciting

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-SESSION_BEFORE = ""// ["(session", "", ""].join("\n");
-SESSION_AFTER  = ""//")"
-function preprocess (data) {
-  return SESSION_BEFORE + data + SESSION_AFTER;
-}
-
-
 STATE_INIT = "init";
 STATE_USE  = "use";
 STATE_FN   = "fn";
@@ -12,88 +5,125 @@ STATE_DEF  = "def";
 KEYWORDS   = ["use", "fn", "def"];
 var ast      = require('../wisp/ast.js')
   , sequence = require('../wisp/sequence.js');
-function processForms (forms) {
+function processForms (read) {
 
-  var output = []
+  if (read.error) throw new Error("Reader error: " + read.error);
+
+  var forms  = read.forms
+    , output = []
     , error  = []
     , state  = STATE_INIT
-    , arg    = null;
+    , arg    = null
+    , meta   = null;
 
   function isSymbol(f) {
-    return f.constructor.type === 'wisp.symbol' && f.namespace === undefined;
+    return f.metadata.type === 'wisp.symbol' && f.namespace === undefined;
   }
 
-  function exitState(val) {
-    if (val) output.push(val);
+  function exitState(val, meta) {
+    if (meta) val = ast.withMeta(val, meta);
+    output.push(val);
     state = STATE_INIT;
-    arg = null;
+    arg   = null;
+    meta  = null;
+  }
+
+  function unexpectedForm(f, i) {
+    throw new Error('Unexpected form ' + i + ': ' + f +
+      (f.metadata ? (' at ' + f.metadata.start.line
+                      + ':' + f.metadata.start.column)
+                  : ''));
+  }
+
+  function enableMetadata(f) {
+    Object.defineProperty(f, 'metadata', { enumerable: true });
+    f.metadata.type = f.constructor.type
+    return f;
   }
 
   for (var i = 0; i < forms.length; i++) {
     var form = forms[i]
       , type = form.constructor.type;
 
-    console.log(form);
+    form = enableMetadata(form);
+
+    console.log("->", form);
 
     if (state === STATE_INIT) {
+      console.log('init', form, isSymbol(form));
+
       if (isSymbol(form) && form.name === 'use') {
         state = STATE_USE;
+        meta  = form.metadata;
       } else if (isSymbol(form) && form.name === 'fn') {
         state = STATE_FN;
+        meta  = form.metadata;
       } else if (isSymbol(form)) {
         state = STATE_DEF;
+        meta  = form.metadata;
         arg   = form;
-      }
+      } else unexpectedForm(form, i);
+
     } else {
+
       if (isSymbol(form) && KEYWORDS.indexOf(form.name) != -1) {
-        throw new Exception("Unexpected form: " + form.name);
+        unexpectedForm(form, i);
       }
       if (state === STATE_USE) {
         exitState(sequence.list(
           ast.symbol(undefined, 'use'),
-          form));
+          form), meta);
       } else if (state === STATE_FN) {
         if (arg === null) arg = form;
         else exitState(sequence.list(
           ast.symbol(undefined, 'defn'),
-          arg, form));
+          arg, form), meta);
       } else if (state === STATE_DEF) {
         exitState(sequence.list(
           ast.symbol(undefined, 'def'),
-          arg, form));
+          arg, form), meta);
       }
+
     }
 
   }
 
-  console.log("\nOUTPUT", output)
   return output;
 
 }
 
 
-var wisp = require("../wisp/compiler.js");
+function makePage(forms) {
+  return '' + 
+    '<head><style>* { font-family: sans-serif; color: #555; }</style></head>' +
+    '<body><script>var forms = ' + JSON.stringify(forms) + '</script>' + 
+    '<script>forms.map(function(f){ ' +
+    'var el = document.createElement("div"); console.log(JSON.serialize(f));' +
+    ' el.innerHTML = f; document.body.appendChild(el)})</script></body>';
+}
+
+
+var wisp     = require("../wisp/compiler.js")
+  , http     = require("http")
+  , sendHTML = require("send-data/html");
 function loadFile (err, data) {
 
-  // wisp code
-  if (err) throw err;
-  data = preprocess(data);
-
   // wisp ast
-  var read  = wisp.readForms(data, 'main.wisp')
-    , forms = processForms(read.forms)
-    , error = read.error;
-
-  if (error) console.log("ERROR:", error, "\n");
-  forms.map(function (f) { console.log(f) } );
+  var forms = processForms(wisp.readForms(data, 'main.wisp'))
 
   // js ast
   var ast = wisp.analyzeForms(forms);
   console.log("\n", ast);
 
   // js code
-  var output = wisp.generate.bind(null, {}).apply(null, ast.ast);
+  var options = { "source-uri": "foobar", "source": data };
+  var output = wisp.generate.bind(null, options).apply(null, ast.ast);
   console.log("\n", output);
+
+  // live editor
+  http.createServer(function (req, res) {
+    sendHTML(req, res, { body: makePage(forms) });
+  }).listen("4194");
 
 }
 
--- main.wisp
+++ main.wisp
@@ -1,4 +1,4 @@
-use sampler
+use postmelodic
 use jack
 use web
 
@@ -6,16 +6,12 @@ samplers
   []
 
 fn add-sampler
-  ; creates a new instance of postmelodic and adds it to the gui
-  ([client-name port-number] (add-sampler samplers.length client-name port-number))
-  ;([after client-name port-number])
+  ([index client-name port-number] (add-sampler samplers.length client-name port-number))
 
 fn remove-sampler
-  ; kills the specified instance of postmelodic and removes it from the gui
   ([index])
 
 fn send-osc
-  ; relays an osc message to the specified instance of postmelodic
   ([index address & args])
 
 server

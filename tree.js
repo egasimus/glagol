e3999313edb73e2a6f7735584de475065c9b71e5
Adam Avramov <fallenblood@gmail.com>
2015-07-14 20:19:20 +0300

    just use plain browserify.require

--- tree.js
+++ tree.js
@@ -2,19 +2,15 @@ var runtime = require('./runtime.js');
 var log = require('./logging.js').getLogger('trees');
 var engine = runtime.requireWisp('./engine.wisp');
 var web = runtime.requireWisp('lib/web.wisp');
-var fs = require('fs');
 var path = require('path');
-var mdeps = require('module-deps');
-
+var browserify = require('browserify');
+var shortid = require("shortid");
 var resolve = function (path) {
   return require('resolve').sync(path,
     { basedir:    './project'
     , extensions: [".js", ".wisp"] });
 }
 
-var pack = require("browser-pack");
-var streamString = require("stream-string");
-var shortid = require("shortid");
 var deps = [];
 
 engine.start('./project').then(function (args) {
@@ -45,78 +41,29 @@ engine.start('./project').then(function (args) {
   })
 
   console.log();
-  log(requires);
+  log("requires\n", requires);
   console.log();
-  log(resolved);
+  log("resolved\n", resolved);
   console.log();
-  log(mapped);
+  log("mapped\n",   mapped);
+
+  var template =
+    [ "var %%%IN HERE LADS%%%"
+    , ";var deps="
+    , JSON.stringify(mapped)
+    , ";exports=function getRequire(a){"
+    , "return function atomRequire(m){"
+    , "return require(deps[a][m])}}"
+    ].join("");
 
   console.log();
-  var source = "var deps=" + JSON.stringify(mapped) + ";"
-    + "exports=function getRequire(a){"
-    + "return function require(m){"
-    + "return require(deps[a][m])}}";
-  var packed = pack({raw: true});
-  var ids = {};
-  Object.keys(resolved).map(function (i) {
-    var source = fs.readFileSync(i, 'utf8');
-    if (path.extname(i) === '.wisp') {
-      source = runtime.compileSource(source, i).output.code;
-    }
-    var md = mdeps({ transform: [web.wispify] });
-    md.end({ file: i });
-    streamString(md).then(function (data) {
-      log("MD", i, JSON.stringify(data));
-    }).error(function (err) { throw err });
-    packed.write({ id: resolved[i], source: source})
-    ids[resolved[i]] = resolved[i];
+  var br = browserify();
+  br.transform(web.wispify);
+  Object.keys(resolved).map(function (module) {
+    br.require(module, { expose: resolved[module] });
+  })
+  br.bundle(function (err, buf) {
+    log(template.replace("%%%IN HERE LADS%%%", String(buf)));
   });
-  packed.end({ id: '____', source: source, deps: ids });
-  streamString(packed).then(function (data) {
-    console.log(data);
-  }).error(function (err) { throw err });
 
 }).done();
-
-/*
-
-function () {
-
-var init
-  , template
-  , samplerTemplate;
-
-var deref = function deref (atom) {
-  return atom.value();
-}
-
-var requireMap = (function () {
-  var modules = {
-    VJROHD3O: { init: function () ... }
-  }
-  return function requireMap (map) {
-    return function require (name) {
-      return modules[map[name]];
-    }
-  }
-})();
-
-// this shit should go in a vm iframe
-init = (function (require, exports) {
-  var vdom = require("../lib/vdom.wisp");
-  exports = vdom.init(script.parentElement, deref(template));
-})(requireMap({"..lib/vdom.wisp":"VJROHD3O"}), {});
-
-template = (function (require) {
-  var vdom = require("../lib/vdom.wisp");
-  exports = function templateApp () {
-    return vdom.h(...);
-  }
-})(requireMap({"..lib/vdom.wisp":"VJROHD3O"}), {});
-
-init();
-
-}
-
-*/
-

77ad69fb6c51f9fd7dc78ef06a1784bc55145e01
Adam Avramov <fallenblood@gmail.com>
2015-07-15 22:37:55 +0300

    continue trying to make this work

--- harness.js
+++ harness.js
@@ -11,10 +11,22 @@ var getRequire = (function(){
 
   var ATOMS = {};
 
+  function deref (atomName) {
+    console.log("deref", atomName);
+    if (!ATOMS[atomName].value) {
+      ATOMS[atomName].value =
+        require('vm').runInNewContext(ATOMS[atomName].compiled, makeContext(atomName));
+    }
+    return ATOMS[atomName].value;
+  }
+
   function makeContext (atomName) {
-    console.log(document.currentScript);
-    return { require: getRequire(atomName)
-           , script:  document.currentScript };
+    var context =
+      { require: getRequire(atomName)
+      , script:  document.getElementsByTagName("script")[0] // TODO
+      , deref:   deref };
+
+    return context;
   };
 
   var req = new XMLHttpRequest();
@@ -24,7 +36,8 @@ var getRequire = (function(){
       ATOMS[atom] = atoms[atom];
     })
     console.log(ATOMS);
-    require('vm').runInNewContext(ATOMS[atomName].compiled, makeContext(atomName));
+    ATOMS[atomName].value =
+      require('vm').runInNewContext(ATOMS[atomName].compiled, makeContext(atomName));
   }
   req.open("get", "/atoms", true);
   req.send();

8066fdc3809436a0a2a6d1e4bf59af9747a295a0
Adam Avramov <fallenblood@gmail.com>
2015-08-04 20:00:43 +0300

    enable log.as

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 var colors = require('colors/safe')
   , pretty = require('prettyjson');
 
-var getLogger = exports.getLogger = function getLogger (from) {
-  return function logger () {
-    var args    = []
-      , output  = ""
-      , newline = false;
+function logger (from) {
+  var args    = []
+    , output  = ""
+    , newline = false;
 
-    for (var i = 0; i < arguments.length; i++) {
-      var arg = arguments[i];
-      if (typeof arg === 'object') {
-        if (i === 0) args.push("\n");
-        args.push(pretty.render(filterObject(arg)));
-        if (i < arguments.length - 1) args.push("\n");
-      } else {
-        args.push(typeof arg === 'undefined' ? '<undefined>' : arg);
-        args.push(" ");
-      }
+  for (var i = 1; i < arguments.length; i++) {
+    var arg = arguments[i];
+    if (typeof arg === 'object') {
+      if (i === 0) args.push("\n");
+      args.push(pretty.render(filterObject(arg)));
+      if (i < arguments.length - 1) args.push("\n");
+    } else {
+      args.push(typeof arg === 'undefined' ? '<undefined>' : arg);
+      args.push(" ");
     }
-
-    output = args.join("");
-
-    if (typeof arguments[0] === 'string' && arguments[0][0] === "\n") {
-      output = output.substr(1);
-      newline = true
-    }
-
-    console.log((newline ? "\n" : "") + colors.yellow(from), output);
   }
+
+  output = args.join("");
+
+  if (typeof arguments[0] === 'string' && arguments[0][0] === "\n") {
+    output = output.substr(1);
+    newline = true;
+  }
+
+  console.log((newline ? "\n" : "") + colors.yellow(from), output);
+}
+
+var getLogger = exports.getLogger = function getLogger (from) {
+  var _logger = logger.bind(null, from);
+  _logger.as = logger;
+  return _logger;
 }
 
 var filterObject = exports.filterObject = function filterObject (obj) {

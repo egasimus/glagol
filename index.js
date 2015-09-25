683abdc82c2321aaba80a4d7fba5693e179718ef
Adam Avramov <fallenblood@gmail.com>
2015-09-25 21:40:31 +0300

    export blessed library and widgets from index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// export notion-based templating system from ./lib
 var e = require('etude-engine');
 var p = require('path');
 var n = e.tree.makeNotionDirectory(p.join(__dirname, 'lib'));
 n.name = require('./package.json').name;
 module.exports = e.notion.getTree(n);
+
+// also export blessed library
+var blessed = require('blessed');
+module.exports.blessed = blessed;
+// and its widgets for convenient access
+// but only under CamelCased names
+Object.keys(blessed.widget)
+  .filter(function(k){ return k[0] === k[0].toUpperCase();    })
+  .map(   function(k){ module.exports[k] = blessed.widget[k]; });

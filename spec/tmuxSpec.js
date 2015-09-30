132a61d1d87f11d8b02de153f19862cdccabfd43
Adam Avramov <fallenblood@gmail.com>
2015-09-30 18:55:58 +0300

    start migrating to etude and tdd

---
+++ spec/tmuxSpec.js
@@ -0,0 +1,21 @@
+describe("tmux layout parser", function () {
+
+  var layouts = {
+    "227x62,0,0{113x62,0,0,13,113x62,114,0,14}": {}
+  };
+
+  for (var l in layouts) {
+    it('can parse ' + l, testCanParse(l));
+  }
+
+  function testCanParse(layout) {
+    return function () {
+      expect(compareTrees(require('../parse')(l), layouts[l])).toBe(true);
+    };
+  }
+
+  function compareTrees(a, b) {
+    return false;
+  }
+
+});

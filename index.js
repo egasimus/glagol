bed50a8505cf9aa597f51a8b41f70cbc21fa41ff
Adam Avramov <fallenblood@gmail.com>
2015-09-30 18:27:42 +0300

    generate sorta-working layouts

--- index.js
+++ index.js
@@ -21,14 +21,15 @@ function splitChildren (children) {
 function parse (layout) {
   var re   = /(\d+x\d+),(\d+),(\d+)(,\d+|{[\dx,\[\]\{\}]+})/
     , l    = re.exec(layout)
+
     , node = {
         width:    Number(l[1].split('x')[0]),
         height:   Number(l[1].split('x')[1]),
         offset:   Number(l[2]),
         wtf:      Number(l[3]),
+        window:   Number(l[4].slice(1)) || null,
         split:    null,
-        children: [],
-        window:   null
+        children: []
       };
 
   if (isNaN(Number(l[4]))) {
@@ -62,6 +63,48 @@ function shrinkX (layout, amount, offset) {
   }
 }
 
-layout = "227x62,0,0{113x62,0,0,13,113x62,114,0,14}";
+function addLeftPane (layout, width) {
+  return {
+    width:    layout.width,
+    height:   layout.height,
+    offset:   layout.offset,
+    wtf:      layout.wtf,
+    split:    "vertical",
+    children: [ { width:    width
+                , height:   layout.height
+                , offset:   0
+                , wtf:      0
+                , split:    "vertical"
+                , children: []
+                , window:   0 }
+              , shrinkX(layout, width, -(width + 1)) ],
+    window:   null
+  }
+  return shrinkX (layout, width);
+}
 
-console.log(shrinkX(parse(layout), 10));
+function write (layout) {
+  var output = layout.width + "x" + layout.height
+          + "," + layout.offset + "," + layout.wtf;
+
+  if (layout.children.length > 0) {
+    if (layout.split === "vertical") {
+      return output + "{" + layout.children.map(write).join(",") + "}";
+    } else if (layout.split === "horizontal") {
+      return output + "[" + layout.children.map(write).join(",") + "]";
+    } else err("invalid layout split: " + layout.split + " in ");
+  } else if (layout.window !== null) {
+    return output + "," + layout.window
+  } else {
+    err("invalid layout tree: ");
+  }
+
+  function err (txt) {
+    throw new Error(txt + JSON.stringify(layout))
+  }
+}
+
+inputLayout  = "227x62,0,0{113x62,0,0,13,113x62,114,0,14}";
+outputLayout = write(addLeftPane(parse(inputLayout), 30))
+
+console.log(checksum(outputLayout) + "," + outputLayout);

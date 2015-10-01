69af19cb74b26504e73c2855f75119a59932d54e
Adam Avramov <fallenblood@gmail.com>
2015-10-01 23:17:54 +0300

    add some more basic test cases

--- spec/tmuxSpec.js
+++ spec/tmuxSpec.js
@@ -3,11 +3,22 @@ var tmux = require('..');
 describe("tmux layout parser", function () {
 
   var layouts =
-    { "227x62,0,0{113x62,0,0,13,113x62,114,0,14}":
+
+    { "227x62,0,0,3":
+      { width: 226, height: 62, offset: 0, wtf: 0, window: 3 }
+
+    , "227x62,0,0{113x62,0,0,13,113x62,114,0,14}":
       { width: 227, height: 62, offset: 0, wtf: 0
       , split: "vertical", children:
         [ { width: 113, height: 62, offset: 0,   wtf: 0, window: 13 }
         , { width: 113, height: 62, offset: 114, wtf: 0, window: 14 } ] }
+
+    , "227x62,0,0[227x31,0,0,3,227x30,0,32,5]":
+      { width: 227, height: 62, offset: 0, wtf: 0
+      , split: "horizontal", children:
+        [ { width: 227, height: 31, offset: 0,  wtf: 0, window: 3 }
+        , { width: 227, height: 30, offset: 32, wtf: 0, window: 5 } ] }
+
     };
 
   for (var l in layouts) {

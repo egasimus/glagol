a220d705b997b0083ca2b9c93cc7d86b85d2e1aa
Adam Avramov <fallenblood@gmail.com>
2015-10-01 23:48:24 +0300

    figure out 'great mystery' of extra datums

--- spec/tmuxSpec.js
+++ spec/tmuxSpec.js
@@ -5,19 +5,19 @@ describe("tmux layout parser", function () {
   var layouts =
 
     { "100x100,0,0,1":
-      { width: 100, height: 100, offset: 0, wtf: 0, window: 1 }
+      { width: 100, height: 100, left: 0, top: 0, window: 1 }
 
     , "150x50,0,0{100x50,0,0,2,50x50,101,0,3}":
-      { width: 150, height: 50, offset: 0, wtf: 0
+      { width: 150, height: 50, left: 0, top: 0
       , split: "vertical", children:
-        [ { width: 100, height: 50, offset: 0,   wtf: 0, window: 2 }
-        , { width: 50,  height: 50, offset: 101, wtf: 0, window: 3 } ] }
+        [ { width: 100, height: 50, left: 0,   top: 0,  window: 2 }
+        , { width: 50,  height: 50, left: 101, top: 0,  window: 3 } ] }
 
     , "72x100,0,0[72x62,0,0,4,72x38,0,63,5]":
-      { width: 72, height: 100, offset: 0, wtf: 0
+      { width: 72, height: 100, left: 0, top: 0
       , split: "horizontal", children:
-        [ { width: 72,  height: 62, offset: 0,   wtf: 0, window: 4 }
-        , { width: 72,  height: 38, offset: 61,  wtf: 0, window: 5 } ] }
+        [ { width: 72,  height: 62, left: 0,   top: 0,  window: 4 }
+        , { width: 72,  height: 38, left: 0,   top: 63, window: 5 } ] }
 
     };
 

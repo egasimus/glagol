d5c77475ee6e37615430153ca8c68ed6d8326a89
Adam Avramov <fallenblood@gmail.com>
2015-10-02 00:24:13 +0300

    fuck sketch regex, start writing finite-state parser

--- spec/tmuxSpec.js
+++ spec/tmuxSpec.js
@@ -8,16 +8,25 @@ describe("tmux layout parser", function () {
       { width: 100, height: 100, left: 0, top: 0, window: 1 }
 
     , "150x50,0,0{100x50,0,0,2,50x50,101,0,3}":
-      { width: 150, height: 50, left: 0, top: 0
+      { width: 150, height:  50, left: 0, top: 0
       , split: "vertical", children:
-        [ { width: 100, height: 50, left: 0,   top: 0,  window: 2 }
-        , { width: 50,  height: 50, left: 101, top: 0,  window: 3 } ] }
+        [ { width: 100, height: 50, left:   0, top:  0, window: 2 }
+        , { width:  50, height: 50, left: 101, top:  0, window: 3 } ] }
 
     , "72x100,0,0[72x62,0,0,4,72x38,0,63,5]":
-      { width: 72, height: 100, left: 0, top: 0
+      { width:  72, height: 100, left: 0, top: 0
       , split: "horizontal", children:
-        [ { width: 72,  height: 62, left: 0,   top: 0,  window: 4 }
-        , { width: 72,  height: 38, left: 0,   top: 63, window: 5 } ] }
+        [ { width:  72, height: 62, left:   0, top:  0, window: 4 }
+        , { width:  72, height: 38, left:   0, top: 63, window: 5 } ] }
+
+    , "40x30,0,0{30x30,0,0[30x10,0,0,6,30x20,0,11,7],10x30,31,0,8}":
+      { width:  40, height:  30, left: 0, top: 0
+      , split: "vertical", children:
+        [ { width: 30,  height: 30, left:   0, top:  0,
+            split: "horizontal", children:
+            [ { width: 30, height: 10, left: 0, top: 0,  window: 6 }
+            , { width: 30, height: 20, left: 0, top: 11, window: 7 } ] }
+        , { width: 10,  height: 38, left:  31, top: 0, window: 8 } ] }
 
     };
 

2486e871015c1c5724ec34df7bf7fb4f598801f0
Adam Avramov <fallenblood@gmail.com>
2015-10-01 23:15:12 +0300

    passes test for basic split layout

--- spec/tmuxSpec.js
+++ spec/tmuxSpec.js
@@ -2,29 +2,40 @@ var tmux = require('..');
 
 describe("tmux layout parser", function () {
 
-  console.log(tmux, tmux.parse);
-
-  var layouts = {
-    "227x62,0,0{113x62,0,0,13,113x62,114,0,14}": {}
-  };
+  var layouts =
+    { "227x62,0,0{113x62,0,0,13,113x62,114,0,14}":
+      { width: 227, height: 62, offset: 0, wtf: 0
+      , split: "vertical", children:
+        [ { width: 113, height: 62, offset: 0,   wtf: 0, window: 13 }
+        , { width: 113, height: 62, offset: 114, wtf: 0, window: 14 } ] }
+    };
 
   for (var l in layouts) {
-    console.log(l);
     it('can parse ' + l, testCanParse(l));
   }
 
   function testCanParse(layout) {
-    console.log(layout);
     return function () {
-      console.log("foo");
-      console.log(tmux.parse(l));
       expect(compareTrees(tmux.parse(l), layouts[l])).toBe(true);
     };
   }
 
   function compareTrees(a, b) {
-    console.log("comparetrees", a, b)
-    return false;
+    return !Object.keys(a).some(function (i) {
+      if (typeof a[i] === 'object') {
+        if (typeof b[i] === 'object') {
+          compareTrees(a[i], b[i]);
+        } else {
+          return true;
+        }
+      } else {
+        if (a[i] === b[i]) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    });
   }
 
 });

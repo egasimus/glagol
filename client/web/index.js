f62952a1968609acd5e72d24257155816ba80f1b
Adam Avramov <fallenblood@gmail.com>
2015-10-09 23:36:18 +0300

    establish q-connection between browser and server

--- client/web/index.js
+++ client/web/index.js
@@ -1,2 +1,12 @@
 document.write('hellow');
-//require('./foo');
+var socket = new WebSocket("ws://localhost:1620");
+var events = new (require('eventemitter2').EventEmitter2)();
+var api = { emit: function emit (evt, arg) { events.emit(evt, arg); } };
+var conn = new (require("q-connection"))(socket, api);
+socket.addEventListener('open', function () {
+  document.write('socket');
+  conn.get('web').invoke('root').then(
+    function (data) { document.write(data); },
+    function (err)  { console.log(err); });
+})
+//require('./foo.wisp');

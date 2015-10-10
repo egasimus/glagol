fb3a680a10e3a8abcec907865bcc28149ad5336e
Adam Avramov <fallenblood@gmail.com>
2015-10-10 17:57:57 +0300

    yay loading bundled code in the browser works

--- server/web/index.js
+++ server/web/index.js
@@ -3,6 +3,7 @@ document.body.style.backgroundColor='#1d1f21';
 document.body.style.color='d5d8d6';
 document.body.style.fontFamily="'Terminess Powerline', Inconsolata, monospace";
 document.body.style.fontSize=8;
+function error (err) { console.log(err); }
 
 document.body.innerText = 'connecting';
 var socket = new WebSocket("ws://localhost:1620");
@@ -11,7 +12,7 @@ var api = { emit: function emit (evt, arg) { events.emit(evt, arg); } };
 var conn = new (require("q-connection"))(socket, api);
 socket.addEventListener('open', function () {
   document.body.innerText = 'connected';
-  conn.get('web').invoke('root').then(start, error);
+  conn.get('web').invoke('root').then(start, error).done();
 });
 function start (data) {
   document.body.innerText = data;
@@ -21,8 +22,23 @@ function start (data) {
   else if (data.type === "FrozenNotionDirectory")
     evaluate(data.notions.main);
 }
-function error (err)  { console.log(err); }
+
 function evaluate (notion) {
-  console.log("EVALUATING", notion);
+  notion.value = require('vm').runInNewContext(
+    notion.code, makeContext(notion));
+  return notion;
 }
-//require('./foo.wisp');
+
+function makeContext (notion) {
+  return {
+      _:            {}
+    , assoc:        require('wisp/sequence.js').assoc
+    , conj:         require('wisp/sequence.js').conj
+    , isEqual:      require('wisp/runtime.js').isEqual
+    , log:          function () { console.log.apply(console, arguments) }
+    , require:      function () {}
+    , setTimeout:   setTimeout
+    , clearTimeout: clearTimeout
+    , WebSocket:    WebSocket
+    , XMLHttpRequest: XMLHttpRequest };
+};

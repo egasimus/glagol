b26b3a1e35eec699d3f0ea14f4459cb9c79d4554
Adam Avramov <fallenblood@gmail.com>
2015-11-22 13:47:31 +0200

    improve error handling for empty files (which should not throw errors but anyway)

--- server/web/client/thaw.js
+++ server/web/client/thaw.js
@@ -1,7 +1,7 @@
 var File      = require('glagol').File
   , Directory = require('glagol').Directory;
 
-module.exports = function thaw (ice, parent) {
+module.exports = function thaw (ice, parent, id) {
 
   if (parent && !(Directory.is(parent)))
     throw ERR_WRONG_PARENT(parent, ice)
@@ -9,11 +9,11 @@ module.exports = function thaw (ice, parent) {
   if (ice.nodes) {
     var node = Directory(ice.name)
     Object.keys(ice.nodes).map(install);
-    function install (i) { node.nodes[i] = thaw(ice.nodes[i], node); }
+    function install (i) { node.nodes[i] = thaw(ice.nodes[i], node, i); }
   } else if (ice.code) {
     var node = File(ice.name, ice.code);
   } else {
-    throw ERR_FOREIGN_BODY(ice);
+    throw ERR_FOREIGN_BODY(ice, parent, id);
   }
 
   node.parent = parent;
@@ -26,7 +26,8 @@ function ERR_WRONG_PARENT (parent, ice) {
   return Error('parent must be instance of Directory')
 }
 
-function ERR_FOREIGN_BODY (ice) {
-  return Error("can't thaw unknown instance in frozen glagol tree: ",
-    + JSON.stringify(ice));
+function ERR_FOREIGN_BODY (ice, parent, id) {
+  return Error(
+    "can't thaw unknown instance " + id +
+    " in frozen glagol tree of " + parent + ": " + JSON.stringify(ice))
 }

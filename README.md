096c677e0b4661d1a8c8f16072fd94f96bb35d96
Adam Avramov <fallenblood@gmail.com>
2015-12-25 16:08:27 +0200

    tabula rasa; break out of etude repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# glagol-web
+
+Client-side loader and web server to use with [Glagol](https://github.com/egasimus/glagol).
---
+++ client/thaw.js
@@ -0,0 +1,33 @@
+var File      = require('glagol').File
+  , Directory = require('glagol').Directory;
+
+module.exports = function thaw (ice, parent, id) {
+
+  if (parent && !(Directory.is(parent)))
+    throw ERR_WRONG_PARENT(parent, ice)
+
+  if (ice.nodes) {
+    var node = Directory(ice.name)
+    Object.keys(ice.nodes).map(install);
+    function install (i) { node.nodes[i] = thaw(ice.nodes[i], node, i); }
+  } else if (ice.code) {
+    var node = File(ice.name, ice.code);
+  } else {
+    throw ERR_FOREIGN_BODY(ice, parent, id);
+  }
+
+  node.parent = parent;
+
+  return node;
+
+}
+
+function ERR_WRONG_PARENT (parent, ice) {
+  return Error('parent must be instance of Directory')
+}
+
+function ERR_FOREIGN_BODY (ice, parent, id) {
+  return Error(
+    "can't thaw unknown instance " + id +
+    " in frozen glagol tree of " + parent.path + ": " + JSON.stringify(ice))
+}

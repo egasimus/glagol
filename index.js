8579135428a5f74125676bd65862efbe5b97de14
Adam Avramov <fallenblood@gmail.com>
2015-12-29 18:39:55 +0200

    make export work, add missing libraries and clean shit up

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = require('glagol')(__dirname,
+  { eslisp: { transform:
+    [ require('eslisp-dotify')
+    , require('eslisp-camelify')
+    , require('eslisp-propertify') ] } }).tree()

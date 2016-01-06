66777e45552a7a5d9490aff83b155e501292df3f
Adam Avramov <fallenblood@gmail.com>
2016-01-07 01:16:00 +0200

    make compatible with new Directory methods

--- client/thaw.js
+++ client/thaw.js
@@ -9,7 +9,7 @@ module.exports = function thaw (ice, parent, id) {
   if (ice.nodes) {
     var node = Directory(ice.name)
     Object.keys(ice.nodes).map(install);
-    function install (i) { node.nodes[i] = thaw(ice.nodes[i], node, i); }
+    function install (i) { node.add(thaw(ice.nodes[i], node, i)); }
   } else if (ice.code) {
     var node = File(ice.name, ice.code);
   } else {

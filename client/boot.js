2a9e6231909f0288b691aeab081ad99b1e37fb6f
Adam Avramov <fallenblood@gmail.com>
2016-01-11 23:39:44 +0200

    add basic live updating and export app object

--- client/boot.js
+++ client/boot.js
@@ -1,8 +1,14 @@
 ;(function(deps, ice){
   var %REQUIRE%;
   require("glagol").require.install(deps, require);
-  var app = require("__glagol_thaw")(ice);
+  var app = window._glagol = require("__glagol_thaw")(ice);
   app.tree().main(app);
   var socket = new WebSocket("ws://" + window.location.host)
-  socket.onmessage = function (msg) { console.log(arguments) }
+  socket.onmessage = function (msg) {
+    var data = JSON.parse(msg.data);
+    if (data.event === "glagol.changed") {
+      console.log("changed", data.path);
+      app.get(data.path).source = data.value;
+    }
+  }
 })(%DEPS%, %ICE%)

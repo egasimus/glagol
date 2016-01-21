09efcec57f8106c49ca0eea67efa5fb2e14235cf
Adam Avramov <fallenblood@gmail.com>
2016-01-21 15:03:35 +0200

    tabula rasa

---
+++ client/api.js
@@ -0,0 +1,13 @@
+(function (getConnection) {
+
+  function apiCall (path) {
+    var args = Array.prototype.slice.call(arguments, 1)
+    console.debug("api call", path, args)
+    return apiCall.getConnection().fcall(path, args)
+  }
+
+  apiCall.getConnection = getConnection;
+
+  return apiCall;
+
+})
---
+++ server/api.js
@@ -0,0 +1,39 @@
+(function (getApi)
+
+  function apiCall (path, args) {
+
+    var api = apiCall.getApi();
+
+    return new Promise(function (win, fail) {
+      try {
+        call(path.split('/'), api)
+      } catch (e) {
+        fail("api: error calling " + path + ": " + e.message)
+      }
+    })
+
+    function call (pathSteps, apiBranch) {
+      if (pathSteps.length < 1) {
+        fail("api: no path specified")
+      } else if (pathSteps.length > 1) {
+        if (Object.keys(apiBranch).indexOf(pathSteps[0]) > -1) {
+          return call(pathSteps.slice(1), apiBranch[pathSteps[0]])
+        } else {
+          fail("api: " + path + " is not in the api")
+        }
+      } else {
+        if (apiBranch[pathSteps[0]] instanceof Function) {
+          win(apiBranch[pathSteps[0]].apply(apiBranch, args))
+        } else {
+          fail("api: " + path + " is not in the api")
+        }
+      }
+    }
+
+  }
+
+  apiCall.getApi = getApi;
+
+  return apiCall;
+
+)

b4e121dd9b6a64668d277ba56d6a180a2bc26f73
Adam Avramov <fallenblood@gmail.com>
2016-02-04 21:08:58 +0200

    add segfault handler

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('segfault-handler').registerHandler("glagol-crash.log");
+
 module.exports           = require('./core/loader.js')();
 module.exports.Loader    = require('./core/loader.js');
 module.exports.File      = require('./core/file.js')

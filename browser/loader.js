168d0f7897e15eb0eafc0a7d7ae17b068f153573
Adam Avramov <fallenblood@gmail.com>
2016-02-08 14:35:47 +0200

    log loader actions in browser console

--- browser/loader.js
+++ browser/loader.js
@@ -9,32 +9,69 @@ function Loader () {
   if (this instanceof Loader) return Loader();
 
   load.nodes  = {};
+  load.update = update;
+  load.remove = remove;
   load.events = new (require('eventemitter3'))();
+  load.events.on('added',   Loader.logAdded);
+  load.events.on('changed', Loader.logChanged);
+  load.events.on('removed', Loader.logRemoved);
 
   return load;
 
   function load (name, source) {
-
     if (name instanceof Object) return load("/", name);
+    if (load.nodes[name]) return update(name, source);
 
-    if (load.nodes[name]) return load.nodes[name];
+    var type = source instanceof Object ? loadDirectory : loadFile;
+    load.nodes[name] = type(name, source);
+    load.events.emit('added', load.nodes[name]);
+    return load.nodes[name];
 
-    var node;
-
-    if (source instanceof Object) {
-      node = Directory(name ? path.basename(name) : null);
+    function loadDirectory (name, source) {
+      var node = Directory(path.basename(name));
       Object.keys(source).map(function (id) {
         node.add(load(path.join(name, id), source[id]));
       });
-    } else {
-      node = File(path.basename(name), source);
+      return node;
     }
 
-    load.nodes[name] = node;
-    load.events.emit('added', node);
-
-    return node;
-
+    function loadFile (name, source) {
+      return File(path.basename(name), source);
+    }
   }
 
+  function update (name, source) {
+    if (!load.nodes[name]) return load(name, source);
+    load.nodes[name].source = source;
+    load.events.emit('changed', load.nodes[name]);
+  }
+
+  function remove (name) {
+    var oldNode = load.nodes[name];
+    if (!oldNode) return;
+
+    var parent = oldNode.parent;
+    if (parent) parent.remove(oldNode);
+    delete load.nodes[name];
+    load.events.emit('removed', oldNode, parent);
+  }
+
+}
+
+Loader.logAdded = function (node) {
+  var type = node.constructor.name.toLowerCase();
+  console.log("%cadded " + type, consoleTag("green"), node.name);
+};
+
+Loader.logChanged = function (node) {
+  console.log("%cchanged", consoleTag("orange"), node.path);
+};
+
+Loader.logRemoved = function (node, parent) {
+  console.log("%cremoved", consoleTag("darkred"),
+    path.join(parent ? parent.path : "", node.name))
+};
+
+function consoleTag (color) {
+  return "background:"+color+";color:white;font-weight:bold;padding:2px 6px"
 }

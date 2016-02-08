d642750c40b23cccb4b5f47d786c8b5060395fec
Adam Avramov <fallenblood@gmail.com>
2016-02-08 14:37:28 +0200

    use core's new built-in client-side loader

--- browser/boot.js
+++ browser/boot.js
@@ -1,62 +1,26 @@
 ;(function(deps, ice){
 
   var %REQUIRE%;
-  var glagol = require('glagol');
-  glagol.require.install(deps, require);
 
-  var app = window._glagol = require("__glagol-thaw__")(ice);
-  app.tree.main(app);
+  var loader = require('glagol')
+    , app = window.Glagol = loader(ice);
+  Glagol.events = loader.events;
+  loader.require.install(deps, require);
+  Glagol.noConflict = function noConflict () {
+    delete window.Glagol;
+    return app;
+  }
+
+  app.tree.main();
 
   var socket = new WebSocket("ws://" + window.location.host);
   socket.onmessage = function (msg) {
     var data = JSON.parse(msg.data);
-
-    if (data.event === "glagol.added") {
-      data.path.split('/').reduce(installNode, app);
-      console.log("%cadded", tag("green"), data.path);
+    switch (data.event) {
+      case "glagol.added":   loader(data.path, data.value);        break;
+      case "glagol.changed": loader.update(data.path, data.value); break;
+      case "glagol.removed": loader.remove(data.path);             break;
     }
-
-    if (data.event === "glagol.changed") {
-      try {
-        app.get(data.path).source = data.value;
-      } catch (e) {
-        data.path.split('/').reduce(installNode, app);
-      }
-      console.log("%cchanged", tag("orange"), data.path);
-    }
-
-    if (data.event === "glagol.removed") {
-      try {
-        var parent = app.get(data.parent);
-      } catch (e) {
-        return;
-      }
-      parent.remove(data.name);
-      console.log("%cremoved", tag("darkred"),
-        (parent ? parent.path + '/' : '') + data.name);
-    }
-
-    function installNode (dir, next, i, steps) {
-      if (next === '' && i === 0) {
-        return dir;
-      }
-
-      if (dir.nodes[next] && glagol.Directory.is(dir.nodes[next])) {
-        return dir.nodes[next];
-      }
-
-      if (i === steps.length - 1 && data.hasOwnProperty("value")) {
-        dir.nodes[next] = glagol.File(next, data.value || "");
-      } else {
-        dir.nodes[next] = glagol.Directory(next);
-      }
-
-      return dir;
-    }
-  };
-
-  function tag (color) {
-    return "background:"+color+";color:white;font-weight:bold;padding:2px 6px"
   }
 
 })(%DEPS%, %ICE%)
--- util/freeze.js
+++ util/freeze.js
@@ -1,48 +1,21 @@
 (function () {
 
-  var glagol = require('glagol');
+  var File      = require('glagol/core/file')
+    , Directory = require('glagol/core/directory')
 
-  return freeze;
+  return function freeze (node) {
 
-  function freeze (node) {
-
-    if (glagol.File.is(node)) {
-      return freezeFile(node)
-    } else if (glagol.Directory.is(node)) {
-      return freezeDirectory(node)
+    if (File.is(node)) {
+      return node.compiled || "";
+    } else if (Directory.is(node)) {
+      var ice = {};
+      Object.keys(node.nodes).forEach(function (name) {
+        ice[name] = freeze(node.nodes[name]);
+      })
+      return ice;
     } else {
-      throw ERR_FOREIGN_BODY(node);
+      throw Error("can't freeze unknown instance in glagol tree");
     }
-
-  }
-
-  function ERR_FOREIGN_BODY (node) {
-    return Error("can't freeze unknown instance in glagol tree");
-  }
-
-  function freezeFile (file) {
-
-    var ice =
-      { name: file.name
-      , time: String(Date.now())
-      , code: file.compiled || "" };
-
-    return ice;
-
-  }
-
-  function freezeDirectory (dir) {
-
-    var ice =
-      { name:  dir.name
-      , time:  String(Date.now())
-      , nodes: {} };
-
-    Object.keys(dir.nodes).map(function freezeNode (k) {
-      ice.nodes[k] = freeze(dir.nodes[k]);
-    }, this);
-
-    return ice;
 
   }
 

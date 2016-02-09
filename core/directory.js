01066475bfc6e269ed96a5e8c61e350b1de6d81a
Adam Avramov <fallenblood@gmail.com>
2016-02-09 10:27:29 +0200

    rework Directory into a teratofunction too

--- core/directory.js
+++ core/directory.js
@@ -2,6 +2,15 @@ var path = require('path');
 
 var Directory = module.exports = function Directory () {
 
+  // this is a factory (see file.js)
+  if (this instanceof Directory) {
+    throw new Error("glagol.Directory is not really a class. " +
+      "Don't use the `new` operator. ")
+  }
+
+  // possible signatures:
+  //   (options)
+  //   (name, options)
   var name, options;
   if (arguments[0] && typeof arguments[0] === 'object') {
     name    = null;
@@ -11,46 +20,64 @@ var Directory = module.exports = function Directory () {
     options = arguments[1] || {};
   }
 
-  // enforce usage of `new` keyword even if omitted
-  if (!(this instanceof Directory)) return new Directory(name, options);
+  // the core of our directory object
+  function directory () { return directory.tree }
 
-  this.name    = name;
-  this.nodes   = {};
-  this.options = options;
-  this.parent  = options.parent || null;
+  // basic info
+  Object.defineProperty(directory, "name", { value: name });
+  directory.name = name;
+  directory.nodes = {};
+  directory.options = options;
+  directory.parent = options.parent || null;
 
-  Object.defineProperties(this,
-    { path:    // path of node relative to app root
-      { configurable: true
-      , enumerable:   true
-      , get: getPath.bind(this) }
-    , tree:    // just nested value objects, no metadata
-      { configurable: true
-      , enumerable:   true
-      , get: getTree.bind(this) }
-    , _glagol: // hidden metadata for duck typing when instanceof fails
+  // bind methods
+  directory.add = add.bind(directory);
+  directory.remove = add.bind(directory);
+  directory.bind = bind.bind(directory);
+  directory.mount = mount.bind(directory);
+  directory.get = get.bind(directory);
+
+  // magic properties
+  Object.defineProperties(directory,
+    { path: descriptor(getPath) // path of node relative to app root
+    , tree: descriptor(getTree) // just nested value objects, no metadata
+    , _glagol: // hidden metadata for duck typing
       { configurable: false
       , enumerable:   false
       , value: { version: require('../package.json').version
                , type:    "Directory" } } });
 
+  // helper for shorter magic property descriptors
+  function descriptor (getter, setter) {
+    var d = { configurable: true, enumerable: true };
+    if (getter) d.get = getter.bind(directory);
+    if (setter) d.set = setter.bind(directory);
+    return d;
+  }
+
+  return directory;
+
 };
 
+Directory.is = function (node) {
+  return (node instanceof Directory ||
+    (node._glagol instanceof Object && node._glagol.type === 'Directory'));
+}
+
 function getPath () {
-  if (this.parent) {
-    return this.parent.path +
-      (this.parent.parent ? '/' : '') +
-      this.name;
-  } else {
-    return '/';
-  }
+  if (!this.parent) return "/";
+
+  var p = this.parent.path;
+  if (this.parent.parent) p += "/"
+  p += this.name;
+  return p;
 }
 
 function getTree () {
   return require('./tree.js')(this);
 }
 
-Directory.prototype.add = function (node) {
+function add (node) {
   // if the new child node is identical to the old one, adding it is a no-op
   // alternatively, the old child is cleanly removed and the new one installed
 
@@ -65,7 +92,7 @@ Directory.prototype.add = function (node) {
   return this;
 }
 
-Directory.prototype.remove = function (nodeOrName) {
+function remove (nodeOrName) {
   // nodes can be removed either by reference or by name
 
   var name = (typeof nodeOrName === 'string') ? nodeOrName : nodeOrName.name;
@@ -75,7 +102,7 @@ Directory.prototype.remove = function (nodeOrName) {
   return this;
 }
 
-Directory.prototype.bind = function () { // TODO nested?
+function bind () { // TODO nested?
   Array.prototype.forEach.call(arguments, function (node) {
     Object.keys(node.nodes).forEach(function (name) {
       this.add(node.nodes[name]);
@@ -85,13 +112,13 @@ Directory.prototype.bind = function () { // TODO nested?
   return this;
 }
 
-Directory.prototype.mount = function () {
+function mount () {
   Array.prototype.forEach.call(arguments, this.add.bind(this));
 
   return this;
 }
 
-Directory.prototype.get = function (relPath) {
+function get (relPath) {
   var node  = this,
       steps = relPath.split("/");
   for (var i = 0; i < steps.length; i++) {
@@ -102,9 +129,4 @@ Directory.prototype.get = function (relPath) {
     if (i < steps.length - 1 && !Directory.is(node)) return null;
   }
   return node;
-}
-
-Directory.is = function (node) {
-  return (node instanceof Directory ||
-    (node._glagol instanceof Object && node._glagol.type === 'Directory'));
 }

84cf86716de8c7c5f7dcab21154eb1298f24adb9
Adam Avramov <fallenblood@gmail.com>
2016-02-16 06:35:01 +0200

    move to external glagol-eslisp dependency

---
+++ server/util/eslisp.js
@@ -0,0 +1,10 @@
+(function () {
+  var eslisp = require('glagol-eslisp')({
+    extraTransformMacros: [
+      require('eslisp-dotify'),
+      require('eslisp-camelify'),
+      require('eslisp-propertify') ] })
+
+  if (!global.Glagol) module.exports = eslisp;
+  return eslisp;
+})()

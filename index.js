92733821dcf92cd3bfbe666bd7c6350846ad20fe
Adam Avramov <fallenblood@gmail.com>
2016-02-16 06:33:01 +0200

    add glagol-eslisp dependency

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = require('glagol')(__dirname,
-  { eslisp: { transform:
+  { formats: { '.esl': require('glagol-eslisp')({ extraTransformMacros:
     [ require('eslisp-dotify')
     , require('eslisp-camelify')
-    , require('eslisp-propertify') ] } })();
+    , require('eslisp-propertify') ] }) } })();

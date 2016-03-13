ed76ddc29cc1da84c5c951f1599471b9347faeac
Adam Avramov <fallenblood@gmail.com>
2016-03-13 19:22:17 +0200

    remove debugging line

--- browser/require.js
+++ browser/require.js
@@ -1,5 +1,4 @@
 module.exports = function (from) {
-  console.log(from, module.exports.deps[from]);
   return function _require (what) {
     return module.exports.require(module.exports.deps[from][what]);
   };

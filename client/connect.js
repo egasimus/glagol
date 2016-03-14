a6a44f3d4fc2636a8fa48b57803124a6e54c3936
Adam Avramov <fallenblood@gmail.com>
2016-03-15 00:22:58 +0200

    actually connect!

--- client/connect.js
+++ client/connect.js
@@ -1,6 +1,12 @@
 (function (state) {
-  state.socket         = new WebSocket("ws://" + window.location.host);
+  state.socket = new WebSocket("ws://" + window.location.host);
   state.socket.onclose = function () { state.server = false };
-  state.socket.onopen  = function () { state.server = true  };
-  state.connection     = require('q-connection')(state.socket);
+  state.socket.onopen = function () {
+    state.socket.send('riko');
+    state.socket.onmessage = function () {
+      state.socket.onmessage = null;
+      state.server = true;
+      state.connection = require('q-connection')(state.socket);
+    }
+  };
 })

c75a0eaae0d7961fd13bd778d15d764d35fac096
Adam Avramov <fallenblood@gmail.com>
2016-03-23 14:33:10 +0200

    successfully connect to api

--- client/connect.js
+++ client/connect.js
@@ -1,12 +1,14 @@
-(function (state) {
+(function (state, connected) {
   state.socket = new WebSocket("ws://" + window.location.host);
-  state.socket.onclose = function () { state.server = false };
+  state.socket.onclose = function () {
+    state.server = false
+  };
   state.socket.onopen = function () {
     state.socket.send('riko');
-    state.socket.onmessage = function () {
-      state.socket.onmessage = null;
-      state.server = true;
-      state.connection = require('q-connection')(state.socket);
-    }
-  };
+    state.server = true;
+  }
+  //state.socket.onmessage = function () {
+    var connection = require('q-connection')(state.socket);
+    connected.resolve({ connection: connection })
+  //}
 })
--- client/init.js
+++ client/init.js
@@ -1,24 +1,36 @@
-(function (root, settings) {
+(function (root, settings, state) {
 
   if (root._glagol.type !== 'Directory' && root.parent) root = root.parent;
 
-  var defaults = { modelName: 'model', globalName: 'API' }
-    , config   = require('extend')(defaults, settings || {})
-    , state    = { root: root, config: config };
+  var defaults  = { modelName:  'model'
+                  , globalName: 'API' }
+    , config    = require('extend')(defaults, settings || {})
+    , connected = require('q').defer()
+    , state     = state ||
+                  { root:       root
+                  , config:     config
+                  , connection: connected.promise };
 
-  Glagol.parent.events.on('changed', init);
+  Glagol.events.once('changed',
+    function (node) {
+      console.log("api initializer changed");
+      var reset = node();
+      reset(root, settings, state) });
+
   init();
 
   return state;
 
   function init () {
     installAPI();
-    _.connect(state); }
+    _.connect(state, connected); }
 
   function installAPI () {
-    var extra = {}
-    extra[config.globalName] = 'API';
-    root._options.globals = require('extend')(root._options.globals, extra) }
+    var API = state.API = getAPI();
+    root._options.globals = function (file) {
+      var g = {};
+      g[config.globalName] = API;
+      return g; }}
 
   function getAPI () {
     return require('riko-api/client')(

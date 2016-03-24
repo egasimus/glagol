4af8a1b56d3d70a230451a7c6eed3cb058215928
Adam Avramov <fallenblood@gmail.com>
2016-03-24 14:47:30 +0200

    connect to remote debug socket

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
 (function () {
-  window.API = $.lib.api.init(Glagol).API;
+  window.API = $.lib.api.init(Glagol).API; // TODO use glagol globals?
   return $.lib.client.init(Glagol);
 })
--- server/api.js
+++ server/api.js
@@ -1,9 +1,20 @@
 module.exports =
   { session:
     { connect:
-        function connect (port) {
-          console.log("connecting to session", port)
-          return new Promise(function (win, fail) {
-            fail('oops')
-          })
-        }  } }
+      function connect (address) {
+        console.log("connecting to debug session at", address);
+        address = (address || '').split(':');
+        var host, port;
+        if (address.length === 1) {
+          host = 'localhost';
+          port = address[0];
+        } else {
+          host = address[0];
+          port = address[1];
+        }
+        return new Promise(function (win, fail) {
+          var address = 'ws://' + (host || 'localhost') + ':' + port
+            , socket  = new (require('ws'))(address);
+          fail('oops')
+        })
+      } } }

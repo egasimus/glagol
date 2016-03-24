f699d04e7de4bce80a7c9b99b62e1d4ed1724a4a
Adam Avramov <fallenblood@gmail.com>
2016-03-24 18:51:55 +0200

    refactor api, start adding subscriber

--- client/commands/connect.js
+++ client/commands/connect.js
@@ -1,10 +1,10 @@
 (function connect (address) {
   console.debug('connect to session', address);
-  API('session/connect', address).done(function (address) {
+  API('connect', address).done(function (address) {
     console.debug("connected to", address)
     App.model.sessions.put(address, { address: address });
     App.model.focusedSession.set(address);
-    API('session/fetch', address).done(function (data) {
+    API('fetch', address).done(function (data) {
       data = JSON.parse(data);
       console.debug("fetched data for", address, data);
       App.model.sessions[address].put('data', data);
--- server/api.js
+++ server/api.js
@@ -1,36 +1,44 @@
 module.exports =
-  { session:
-    { connect:
-      function connect (address) {
-        console.log("connecting to debug session at", address);
-        address = (address || '').split(':');
-        var host, port;
-        if (address.length === 1) {
-          host = 'localhost';
-          port = address[0];
-        } else {
-          host = address[0];
-          port = address[1];
-        }
-        return new Promise(function (win, fail) {
-          var address = 'ws://' + (host || 'localhost') + ':' + port
-            , socket  = new (require('ws'))(address);
-          socket.onerror = fail.bind(null, 'could not connect to ' + address);
-          socket.onopen = function () {
-            socket.onerror = null;
-            if (_.model.sessions[address]) _.model.sessions[address].close();
-            _.model.sessions[address] = socket;
-            win(address);
-          }
-        })
+  { subscribe: function subscribe (cb) {
+      if (cb) _.model(function (val) { cb(serialize(val)) });
+      return serialize(_.model());
+    }
+  , connect: function connect (address) {
+      console.log("connecting to debug session at", address);
+      address = (address || '').split(':');
+      var host, port;
+      if (address.length === 1) {
+        host = 'localhost';
+        port = address[0];
+      } else {
+        host = address[0];
+        port = address[1];
       }
-    , fetch:
-      function fetch (address) {
-        console.log("fetching data for", address);
-        return new Promise(function (win, fail) {
-          win(JSON.stringify(
-            { 'foo/':  '1234'
-            , '  bar': '5678'
-            , '  baz': '9101112' }))
-        })
-      } } }
+      return new Promise(function (win, fail) {
+        var address = 'ws://' + (host || 'localhost') + ':' + port
+          , socket  = new (require('ws'))(address);
+        socket.onerror = fail.bind(null, 'could not connect to ' + address);
+        socket.onopen = function () {
+          socket.onerror = null;
+          if (_.model.sessions[address]) _.model.sessions[address].close();
+          _.model.sessions[address] = socket;
+          win(address);
+        }
+      })
+    }
+  , fetch: function fetch (address) {
+      console.log("fetching data for", address);
+      return new Promise(function (win, fail) {
+        win(JSON.stringify(
+          { 'foo/':  '1234'
+          , '  bar': '5678'
+          , '  baz': '9101112' }))
+      })
+    }
+  };
+
+function serialize (data) {
+  return JSON.stringify(data, function (key, val) {
+    return val;
+  });
+}

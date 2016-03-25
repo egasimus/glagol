f0f3a5e7a4016cdc35043f198369cbf5b47ba47f
Adam Avramov <fallenblood@gmail.com>
2016-03-25 13:45:17 +0200

    make main.js live-editable

--- server/main.js
+++ server/main.js
@@ -1,13 +1,24 @@
-(function () {
+(function (server) {
 
-  var server = _.lib.server.server("0.0.0.0", "1617",
-    function () { return _.routes });
-
-  server.http.on("listening", function () {
-    console.log("listening on 0.0.0.0:1617");
+  Glagol.events.once('changed', function (node) {
+    if (server) {
+      server.http.removeListener("listening", listening);
+      server.sockets.removeListener("connection", connection);
+    }
+    node()(server);
   })
 
-  server.sockets.on("connection", function (socket) {
+  server = server || _.lib.server.server("0.0.0.0", "1617",
+    function () { return _.routes });
+  server.http.on("listening", listening);
+  server.sockets.on("connection", connection);
+  return server;
+
+  function listening () {
+    console.log("listening on 0.0.0.0:1617");
+  }
+
+  function connection (socket) {
     socket.onmessage = function dispatch (msg) {
       if (msg.data === "glagol") {
         socket.onmessage = null;
@@ -18,6 +29,6 @@
         _.lib.api.connect(socket, function () { return _.api });
       }
     }
-  })
+  }
 
 })

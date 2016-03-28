f1b235bcc43d6182a089271dffac784c6978da96
Adam Avramov <fallenblood@gmail.com>
2016-03-28 17:36:28 +0300

    tag v0.1.0

--- init.js
+++ init.js
@@ -6,12 +6,15 @@
 
   var App = { root: root };
   overrides = overrides || {};
-  ['model', 'socket', 'style'].map(install.bind(null, false));
-  ['templates', 'events'].map(install.bind(null, true));
-  function install (dir, name) {
-    var defaultName = './' + name + (dir ? '' : '.js');
+  ['model', 'socket'].map(install.bind(null, '.js'));
+  install('.styl', 'style');
+  ['templates', 'events'].map(install.bind(null, ''));
+  function install (ext, name) {
+    var defaultName = './' + name + ext;
     App[name] = App.root.get(overrides[name] || defaultName);
   }
+
+  console.debug("initializing", App);
 
   _.tasks.model(App);
   if (App.socket) _.tasks.socket(App);

2401c4aef7b8202e6010c7871baae72bee12d2f7
Adam Avramov <fallenblood@gmail.com>
2016-04-02 14:58:11 +0300

    add deps and fix connection

--- src/main.js
+++ src/main.js
@@ -2,12 +2,22 @@
 
   var slavePath = require('path').resolve(process.argv[2])
     , port      = process.argv[3] || '1616'
-    , sockets   = new (require("ws").Server)({ port: port });
+    , sockets   = new (require("ws").Server)({ port: port })
+    , socketId  = 0;
+
+  console.log("debugging", slavePath, "at", port);
+
+  var slaveApp = require(slavePath);
 
   sockets.on('connection', function (socket) {
+    var id = socketId++;
+    console.log('socket', id, 'connected');
+    socket.onclose = function () {
+      console.log('socket', id, 'closed');
+    }
     socket.onmessage = function (msg) {
       socket.onmessage = null;
-      _.server.connect(socket, _.api);
+      _.server.connect(socket, function () { return slaveApp });
     }
   })
 

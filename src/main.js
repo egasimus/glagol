75fb96bf01ae4bd295e0aa9b36737cea56a0f367
Adam Avramov <fallenblood@gmail.com>
2016-04-04 01:41:00 +0300

    move stuff into main to fix scoping issues

--- src/main.js
+++ src/main.js
@@ -1,17 +1,49 @@
-(function () {
+(function (model) {
 
+  // debugger state
+  var model = model ||
+    { app:    null
+    , path:   null
+    , port:   null
+    , socket: null };
+
+  // parse path
   var appPath = require('path').resolve(process.argv[2]);
   $.log('loading', appPath);
-  _.model.app.set(require(appPath));
+
+  // load app
+  model.app = require(appPath);
   $.log('loaded app from', appPath);
 
+  // parse port
   var debugPort = process.argv[3] || 1616;
   $.log('listening on port', debugPort);
-  _.model.port.set(debugPort);
+  model.port = debugPort;
 
-  var sockets = new (require("ws").Server)({ port: _.model.port() })
-  sockets.on('connection', _.connect)
+  // start socket server
+  var sockets = new (require("ws").Server)({ port: model.port })
+  sockets.on('connection', connect);
 
-  _.model.app()().main();
+  // launch app TODO async?
+  model.app().main();
+
+  // socket numbers
+  var socketId = 0;
+
+  function connect (socket) {
+    // get socket id
+    var id = socketId++;
+    $.log('socket open', id);
+
+    // set close handler
+    socket.onclose = function () { console.log('socket close', id); }
+
+    // expose app for remote access
+    var connection = require('q-connection')(socket,
+      function () { return model.app });
+  }
+
+  // return state
+  return model;
 
 })
--- src/model.js
+++
@@ -1,3 +0,0 @@
-( { app:    require('observ')(null)
-  , port:   require('observ')(null)
-  , socket: require('observ')(null) } )

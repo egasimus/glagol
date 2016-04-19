654517ecfeeb760ec416ed66c93b909a74e33f1a
Adam Avramov <fallenblood@gmail.com>
2016-04-20 00:37:44 +0300

    print results of compilation and evaluation

--- src/api.js
+++ src/api.js
@@ -8,27 +8,26 @@
       socket.send('hi');
     }
 
-    var url
-      , root;
-
     if (message.data === 'subscribe') {
-      url  = socket.upgradeReq.url;
-      root = state.app.get(url);
+      var url  = socket.upgradeReq.url;
+      state.cwd = state.app.get(url);
       update();
     }
 
     if (message.data.indexOf('compile') === 0) {
-      root.get(message.data.split(' ')[1].trim()).compiled;
+      var path = message.data.split(' ')[1].trim().slice(1);
+      console.log(state.cwd.get(path).compiled);
       update();
     }
 
     if (message.data.indexOf('evaluate') === 0) {
-      root.get(message.data.split(' ')[1].trim()).value;
+      var path = message.data.split(' ')[1].trim().slice(1);
+      console.log(state.cwd.get(path).value);
       update();
     }
 
     function update () {
-      socket.send('update%' + JSON.stringify(_.serialize(root)));
+      socket.send('update%' + JSON.stringify(_.serialize(state.cwd)));
     }
 
   }

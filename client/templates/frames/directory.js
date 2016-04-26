646176e8fdbaf90a686e11ef3dbc6026e9b4b1e5
Adam Avramov <fallenblood@gmail.com>
2016-04-26 08:42:30 +0300

    rudimentary directory browsing

--- client/templates/frames/directory.js
+++ client/templates/frames/directory.js
@@ -2,12 +2,23 @@
 
   var directory = App.model.directories()[frame.address];
 
-  return h('.Directory', directory.map(entry))
+  return h('.Directory', (directory || []).map(entry))
 
   function entry (data) {
     var isDir = !!(data.stat.mode & 0040000);
     return h('.DirectoryEntry',
-      isDir ? h('strong', data.name_+'/') : data.name_)
+      { onclick: open(data.name_, isDir) },
+      isDir
+        ? h('strong', data.name_+'/')
+        : [ data.name_, ' ', h('em', data.stat.size + ' b') ])
+  }
+
+  function open (name, isDir) {
+    return function (event) {
+      event.preventDefault();
+      var location = require('path').join(frame.address, name);
+      if (isDir) $.commands.add('directory', location);
+    }
   }
 
 })

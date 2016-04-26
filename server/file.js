98b58a0dd121c11e71522e0b15e9dae9902b3d55
Adam Avramov <fallenblood@gmail.com>
2016-04-26 22:56:38 +0300

    add file serving route

--- server/file.js
+++ server/file.js
@@ -1,3 +1,24 @@
-(function (request, response) {
-  require('send-data')(request, response, 'ok');
-})
+var fs        = require('fs')
+  , os        = require('os')
+  , path      = require('path')
+  , sendData  = require('send-data')
+  , sendError = require('send-data/error');
+
+module.exports = function (request, response) {
+  var fs = require('fs')
+  require('fs').readFile(getPath(request), function (err, data) {
+    if (err) {
+      sendError(request, response, { body: err })
+    } else {
+      sendData(request, response, { statusCode: 200, body: data })
+    }
+  })
+};
+
+function getPath (request) {
+  var url  = request.url
+    , re   = new RegExp('/file/(.+)')
+    , file = decodeURI(re.exec(url)[1]);
+  if (file.indexOf('~/') === 0) file = path.resolve(os.homedir(), file.slice(2));
+  return file;
+}

0fbc0f15f36e184c1874b67bbf71fa936bf1db5f
Adam Avramov <fallenblood@gmail.com>
2016-04-30 14:59:57 +0300

    show media player for audio files

--- client/templates/frames/file.js
+++ client/templates/frames/file.js
@@ -1,7 +1,28 @@
 (function (frame, index) {
 
-  console.log(App.model.files()[frame.address]);
+  var file = App.model.files()[frame.address];
 
-  return h('.File', h('img', { src: '/file/' + frame.address }));
+  var body = 'unknown file type: ' + file.type;
+
+  switch (file.type) {
+    case 'audio/mpeg':
+    case 'audio/x-wav':
+      body = h('audio', { controls: true })
+      addSrc();
+      break;
+    case 'image/png':
+    case 'image/jpeg':
+      body = h('img')
+      addSrc();
+      break;
+  }
+
+  console.log(body)
+
+  return h('.File', body);
+
+  function addSrc () {
+    body.properties.src = '/file/' + file.name_;
+  }
 
 })

8d500ab058fd52421f8aa3f7b30de72a40bde12e
Adam Avramov <fallenblood@gmail.com>
2016-05-02 23:18:36 +0300

    refactor main and add fs socket

--- client/main.js
+++ client/main.js
@@ -1,39 +1,51 @@
-(function () {
+(function (noGlobals) {
 
-  // init client framework
-  window.App = $.lib.client.init(Glagol);
+  var modules =
+    { App: initView()
+    , API: initAPI()
+    , FS:  initFS() };
 
+  if (!noGlobals) {
+    Object.keys(modules).forEach(function (id) {
+      window[id] = modules[id];
+    })
+  }
+
+  return modules;
+
+})
+
+function initView () {
+  var App = $.lib.gui.init(Glagol);
   // inject codemirror stylesheet; TODO from node_modules
   var codemirrorStyle = document.createElement('link');
   codemirrorStyle.rel  = 'stylesheet';
   codemirrorStyle.href = 'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.13.4/codemirror.css';
   document.head.appendChild(codemirrorStyle);
+  return App;
+}
 
-  // init API
+function initAPI () {
   // TODO automatically add API to Glagol pseudo-globals instead of the window
   // object. The problem here is that modifying options objects is tricky; and
   // furthermore does not automatically invalidate evaluation caches. Things
   // to fix in Glagol core.
-  var API    = $.lib.api.init(Glagol)
-    , socket = API.socket
-    , API    = window.API = API.API;
-
+  var _API   = $.lib.api.init(Glagol)
+    , socket = _API.socket;
   // reload page when socket closes
   socket.onclose = function () { window.location.reload() }
-
   // subscribe to server api
   // TODO replace with new api framework
-  API('subscribe', function (data) {
-    _.commands.update(data)
-  }).done(function (data) {
-    console.debug("subscribed to server");
-    _.commands.update(data);
-  });
+  _API.API('subscribe', function (data) { _.commands.update(data) })
+    .done(function (data) {
+      console.debug("subscribed to server");
+      _.commands.update(data);
+    });
+  return _API.API;
+}
 
-  // what these guys have seen prolly can not be unseen
-  return {
-    API: API,
-    App: App,
-  };
-
-})
+function initFS () {
+  var FSSocket = new WebSocket('ws://localhost:1615')
+    , FS       = require('riko-api2')(FSSocket)
+  return FS;
+}

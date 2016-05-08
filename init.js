20172dd637716b9a75f3f6f7ac1391b7a40fffe3
Adam Avramov <fallenblood@gmail.com>
2016-05-08 13:18:23 +0300

    prettier code

--- init.js
+++ init.js
@@ -4,12 +4,13 @@
   Glagol.events.on('changed', reload);
   root.events.on('changed', reload);
 
-  var App = { Source: root };
-  App.Model = _.tasks.model(root, modules);
-  App.Style = _.tasks.style(root);
-  App.View  = _.tasks.view(App.Model, root, modules);
+  var App = {}
+  App.Source = root;
+  App.Model  = _.tasks.model(root, modules);
+  App.Style  = _.tasks.style(root);
+  App.View   = _.tasks.view(App.Model, root, modules);
 
-  console.debug("initializing", App);
+  console.debug("initialized", App);
 
   return App;
 

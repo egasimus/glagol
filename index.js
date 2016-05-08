ea63a497eabf53130e355457c28d252a0f325459
Adam Avramov <fallenblood@gmail.com>
2016-05-08 17:02:36 +0300

    simplify further

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-var Router = module.exports = function Router (routes, handlers, default) {
+var Router = module.exports = function Router (routes, options) {
 
-  router.routes   = routes.map(Router.prepareRoute) || [];
-  router.handlers = handlers                        || {};
-  router.default  = default                         || Router.defaultRoute;
-
+  router.prepare  = options.prepare  || default.prepare;
+  router.handler  = options.handler  || default.handler;
+  router.catchall = options.catchall || default.catchall;
+  router.routes   = routers.map(router.prepare);
   return router;
 
   function router (input, data) {
 
-    var result = undefined;
+    var result  = undefined
+      , matched = false;
 
-    if (router.routes.some(function (route, i) {
+    matched = router.routes.some(function (route, i) {
       var match = route[0];
       if (match(input, data)) {
-        result = route[1](input, data);
+        result = router.handler(route[1], input, data);
         return true;
       }
-    })) {
-      return result;
-    } else {
-      return router.default(input, data);
-    }
+    })
+
+    return matcher ? result : router.catchall(input, data);
     
   }
 
 }
 
-Router.defaultRoute = function (input, data) {
-  console.error('with (', data, ') no match for:', input);
-}
-
-Router.prepareRoute = function (route) {
-  var newRoute = [ route[0], route[1] ];
-  if (route[0] instanceof RegExp) newRoute[0] = Router.regExpMatcher(route[0]);
-  if (route[0] instanceof String) newRoute[0] = Router.stringMatcher(route[0]);
-  if (route[1] instanceof String) newRoute[1] = Router.handler(route[1]);
-}
-
-Router.handler = function (name) {
-  return function (input, data) {
-    console.info(input, 'with (', data, ') matched:', name);
-    if (router.handlers[name]) {
-      router.handlers[name](input, data);
-    } else {
-      console.warn('with (', data, ') no handler:', name);
+Router.default =
+  { prepare: function (route) {
+      var newRoute = [ route[0], route[1] ];
+      if (typeof route[0] === 'string') newRoute[0] = Router.stringMatcher(route[0]);
+      if (route[0] instanceof RegExp)   newRoute[0] = Router.regExpMatcher(route[0]);
     }
+  , handler: function (route, input, data) {
+      return route(input, data);
+    }
+  , catchall: function (input, data) {
+      console.error('with (', data, ') no match for:', input);
+    }
+
+Router.debugHandler = function (input, data) {
+  console.info(input, 'with (', data, ') matched:', name);
+  if (router.handlers[name]) {
+    router.handlers[name](input, data);
+  } else {
+    console.warn('with (', data, ') no handler:', name);
   }
 }
 
 Router.regExpMatcher = function (string) {
   return function (input) {
-    input = input.trim();
     var r = new RegExp(string);
     console.debug('match', input.trim(), 'against', r, r.test(input))
     return r.test(input);
@@ -59,7 +57,6 @@ Router.regExpMatcher = function (string) {
 
 Router.stringMatcher = function (string) {
   return function (input) {
-    input = input.trim();
     console.debug('match', input, 'against', string, input === string)
     return input === string;
   }

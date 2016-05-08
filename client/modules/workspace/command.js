ce932c437ed1c9029c9f7095c3692de9864dc86f
Adam Avramov <fallenblood@gmail.com>
2016-05-08 17:01:09 +0300

    reimplement routes with riko-route

---
+++ client/modules/workspace/command.js
@@ -0,0 +1,17 @@
+var command = module.exports = require('riko-route')(
+  [ [ /([0-9])/,            'port'  ]
+  , [ /ws:\/\/(.+):(\d+)/,  'ws'    ]
+  , [ /wss:\/\/(.+):(\d+)/, 'wss'   ]
+  , [ /http:\/\/(.+)/,      'http'  ]
+  , [ /https:\/\/(.+)/,     'https' ]
+  , [ /~(?:\/(.+))?/,       'home'  ]
+  , [ /\/(.*)/,             'root'  ] ] )
+
+command.handler = function (route, input) {
+  return command.handlers[route](input);
+}
+
+command.handlers =
+  { home: function (input) { _.add('directory', input) }
+  , root: function (input) { _.add('directory', input) }
+  , port: function (input) { _.add('glagol', 'ws://localhost:' + input) } };

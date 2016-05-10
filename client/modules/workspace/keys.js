908309b7b5c9e5f91997d4017ec54e87f1e2eab9
Adam Avramov <fallenblood@gmail.com>
2016-05-11 01:40:45 +0300

    implement keyboard navigation left/right

--- client/modules/workspace/keys.js
+++ client/modules/workspace/keys.js
@@ -1,7 +1,20 @@
 module.exports =
-  { Alt:
-    { KeyR: function () {
-        App.Model.Workspace.bars.top.show.set(!App.Model.Workspace.bars.top.show());
-      }
-    }
+  { KeyH: go(-1)
+  , KeyL: go(1)
+  , Alt:
+    { KeyR: toggleLauncher } }
+
+function go (by) {
+  return function () {
+    var Model      = App.Model.Workspace
+      , frameCount = Model.frames().length
+      , nextFrame  = Model.focusedFrame() + by;
+    if (nextFrame < 0)           nextFrame += frameCount;
+    if (nextFrame >= frameCount) nextFrame -= frameCount;
+    Model.focusedFrame.set(nextFrame);
   }
+}
+
+function toggleLauncher () {
+  App.Model.Workspace.bars.top.show.set(!App.Model.Workspace.bars.top.show());
+}

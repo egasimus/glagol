a1c48dc7e32156d98faa1779c47a18688d08c863
Adam Avramov <fallenblood@gmail.com>
2016-05-31 21:54:48 +0300

    fix logging prefix

--- server/log.js
+++ server/log.js
@@ -1,6 +1,6 @@
 (function () {
   var args = Array.prototype.slice.call(arguments, 0);
-  args.unshift(require('colors/safe').bold('glagol-inspector:'));
+  args.unshift(require('colors/safe').bold('glagol-workspace:'));
   console.log.apply(console, args);
 })
 

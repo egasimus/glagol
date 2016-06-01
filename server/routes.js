6eee59efb2229079dd55e309e69f9131438d7748
Adam Avramov <fallenblood@gmail.com>
2016-06-02 02:15:03 +0300

    set user-id cookie

--- server/routes.js
+++ server/routes.js
@@ -2,12 +2,18 @@ var route = _.lib.server.route
   , app   = _.lib.bundler.app
   , path  = require('path')
 
-var opts = { formats: { '.styl': require('glagol-stylus')() } }
+var opts   = { formats: { '.styl': require('glagol-stylus')() } }
+  , client = app(opts, path.resolve(__dirname, '../client'));
 
 module.exports =
   [ route(
       '/',
-      app(opts, path.resolve(__dirname, '../client'))),
+      function (req, res) {
+        var cookies = require('cookie').parse(req.headers.cookie || '')
+          , id      = cookies['user-id'];
+        if (!id) res.setHeader('Set-Cookie', 'user-id=' + _.lib.makeId());
+        return client(req, res);
+      }),
   , route(
       re('^/file/(.+)'),
       function (req, res) { return _.file(req, res) }) ];

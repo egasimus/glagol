fbcc67ea17822cd3540c2ac032857f9d556388a2
Adam Avramov <fallenblood@gmail.com>
2016-06-12 16:45:38 +0300

    fix workspace refreshing

--- modules/workspace/server/api.js
+++ modules/workspace/server/api.js
@@ -11,7 +11,7 @@ module.exports = function (data) {
 
     refresh:
       function () {
-        socket.send(serialize($.model()));
+        socket.send(serialize(_.model()));
       },
 
     add:

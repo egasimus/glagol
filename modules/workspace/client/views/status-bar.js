71882b4c63bab8cdce9c83a5a2f8e337c16b52a0
Adam Avramov <fallenblood@gmail.com>
2016-06-12 18:22:34 +0300

    add modules list to status bar

--- modules/workspace/client/views/status-bar.js
+++ modules/workspace/client/views/status-bar.js
@@ -5,7 +5,10 @@
       //h('.StatusBarNavigation',
       //[ h('a.StatusBarButton', 'Console')
       //, h('a.StatusBarButton', 'Tree') ])
-    , h('.StatusBarText', state.Workspace.StatusBar.text)
+    , h('.StatusBar_Text', state.Workspace.StatusBar.text)
+    , h('.StatusBar_Modules', Object.keys($.modules).map(function (moduleName) {
+        return h('.StatusBar_Module', moduleName);
+      }))
     //, h('.StatusBarPresentation',
       //[ h('a.StatusBarButton', '-' )
       //, h('a.StatusBarButton', '12px')

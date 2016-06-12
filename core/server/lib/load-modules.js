acb39eab97b9cd00ed52d7f7c6f3210c741cfbe9
Adam Avramov <fallenblood@gmail.com>
2016-06-12 16:44:22 +0300

    improve module loading

--- core/server/lib/load-modules.js
+++ core/server/lib/load-modules.js
@@ -4,9 +4,13 @@
     , modules = glagol.Directory('modules');
 
   require('fs').readdirSync(rel('modules')).forEach(function (moduleName) {
-    var modulePath = rel('modules/' + moduleName + '/' + subdir);
+    $.log('trying to load module', moduleName);
     try {
-      modules.add(glagol(modulePath));
+      var modulePath = rel('modules/' + moduleName + '/' + subdir)
+        , module     = glagol(modulePath);
+      Object.defineProperty(module, 'name', { value: moduleName }); // rename
+      module.reset(); // after rename
+      modules.add(module);
     } catch (e) {
       $.log('could not load module', modulePath);
       $.log(e);

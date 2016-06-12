6f6c07b893cf16dba4e71f2c892ba7fd0e2da3d4
Adam Avramov <fallenblood@gmail.com>
2016-06-12 10:33:09 +0300

    pretty print frame state

--- modules/workspace/client/maps/types.js
+++ modules/workspace/client/maps/types.js
@@ -1,10 +1,15 @@
-module.exports = require('riko-route')(
-  [ [ 'iframe',    __.views.frames.iframe      ]
-  , [ 'file',      __.__.fs.views.file         ]
-  , [ 'directory', __.__.fs.views.directory    ]
-  , [ 'glagol',    __.__.debugger.views.glagol ]
-  ] ),
+module.exports = require('riko-route')([]);
+  //[ [ 'iframe',    __.views.frames.iframe      ]
+  //, [ 'file',      __.__.fs.views.file         ]
+  //, [ 'directory', __.__.fs.views.directory    ]
+  //, [ 'glagol',    __.__.debugger.views.glagol ]
+  //] );
 
 module.exports.handler = function (match, input, data) {
   return match(data.frame, data.index);
-}
+};
+
+module.exports.catchall = function (input, data) {
+  return h('pre', { style: { padding: '0 9px' } },
+    require('json-align')(data, false, 2));
+};

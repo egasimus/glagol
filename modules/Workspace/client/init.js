93a8381c34caf5d2673ad7529f317c2bf3d2974a
Adam Avramov <fallenblood@gmail.com>
2016-06-16 00:44:25 +0300

    refactor workspace init

--- modules/Workspace/client/init.js
+++ modules/Workspace/client/init.js
@@ -1,13 +1,13 @@
 (function init (App) {
 
-  var socket = App.API.socket;
-  socket.addEventListener('open',
+  // first request data
+  App.API.socket.addEventListener('open',
     function () { App.API('Workspace/Refresh'); })
 
   // bind keyboard listeners
-  ['up', 'down'].forEach(function (direction) {
-    window.addEventListener('key' + direction, function (event) {
-      console.log(event)
-      return _.onKey(App.Model(), direction, event); }) })
+  window.addEventListener('keydown', function (event) {
+    return _.onKey(App.Model(), 'down', event) })
+  window.addEventListener('keyup',   function (event) {
+    return _.onKey(App.Model(), 'up',   event) })
 
 })

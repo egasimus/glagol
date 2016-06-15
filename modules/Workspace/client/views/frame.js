5b1696adab6e7de7704c5186d485ee09019c6fdc
Adam Avramov <fallenblood@gmail.com>
2016-06-16 02:54:25 +0300

    remove some tracing

--- modules/Workspace/client/views/frame.js
+++ modules/Workspace/client/views/frame.js
@@ -1,7 +1,5 @@
 (function (frame, index) {
 
-  console.log(__.maps)
-
   return h('section.Frame' + (frame.error ? '.FrameWithError' : ''),
     { dataset: { type: frame.type, address: frame.address } },
     __.maps.types(frame.type, { frame: frame, index: index }));

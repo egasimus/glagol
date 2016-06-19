a85156aaaf694b6cadad5e00e53122a525c00cec
Adam Avramov <fallenblood@gmail.com>
2016-06-19 21:17:20 +0300

    allow extra arguments to api

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 module.exports = function makeAPI (getCommands) {
 
-  API.getCommands = getCommands;
-  API.read = function (message) { return JSON.parse(message.data) };
+  var API = function API () {
 
-  return API;
+    var commands = API.getCommands.apply(null, arguments);
 
-  function API (state) {
     return function (message) {
-      var data     = API.read(message)
-        , commands = API.getCommands(state)
-        , command  = commands[data[0]];
+      var data    = API.read(message)
+        , command = commands[data[0]];
       if (command) {
         command.apply(commands, data[1] || []);
       } else {
         throw new Error("riko-api2: no command " + data[0]);
       }
     }
-  }
+
+  };
+
+  API.getCommands = getCommands;
+
+  API.read = function (message) { return JSON.parse(message.data) };
+
+  return API;
 
 }

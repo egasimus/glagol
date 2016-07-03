1d1382174c370a2144010a7e9cb29b5b75399a2c
Adam Avramov <fallenblood@gmail.com>
2016-07-03 16:26:28 +0300

    format negative times correctly

--- core/client/lib/format-time.js
+++ core/client/lib/format-time.js
@@ -1,10 +1,11 @@
 (function formatTime (t) {
-  t = Math.round(t * 1000) / 1000;
+  var negative = t < 0;
+  t = Math.round(Math.abs(t) * 1000) / 1000;
   var m  = Math.floor(t / 60)
     , s  = Math.floor(t % 60)
     , ms = Math.floor(t % 1 * 1000);
   m  = m < 10 ? "0" + m : m;
   s  = s < 10 ? "0" + s : s;
   ms = ms < 10 ? ("00" + ms) : ms < 100 ? ("0" + ms) : ms;
-  return m + ':' + s + '.' + ms;
+  return (negative ? '-' : '') + m + ':' + s + '.' + ms;
 })

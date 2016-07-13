e988596a74ff16faee3d81a8efee1cd0ad37e3d2
Adam Avramov <fallenblood@gmail.com>
2016-07-13 22:11:36 +0300

    remove old cmd.js

--- core/client/cmd.js
+++
@@ -1,6 +0,0 @@
-(function (command) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  return function (event) {
-    event.preventDefault();
-    $.commands[command].apply(null,
-      args.concat(Array.prototype.slice.call(arguments))); } })

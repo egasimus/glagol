c9e179302d331dfeb9cb9453e4dae31c5613b43c
Adam Avramov <fallenblood@gmail.com>
2016-07-23 15:23:52 +0300

    unbreak Device, move styles for directory

--- plugins/Device/client/init.js
+++ plugins/Device/client/init.js
@@ -1,7 +1,9 @@
 module.exports = function (App) {
 
-  App.Workspace.registerFrameType('deviceList',   _.views.list);
-  App.Workspace.registerFrameType('deviceDetail', _.views.detail);
+  App.Workspace.registerFrameType('deviceList',
+    function () { return _.views.list.apply(null, arguments) });
+  App.Workspace.registerFrameType('deviceDetail',
+    function () { return _.views.detail.apply(null, arguments) });
 
   App.Workspace.registerMenuItem('Device list',
     function () { App.API('Workspace/Open', 'deviceList') });

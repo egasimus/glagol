7fd3790fe6c1df4d43d5d7655693281919bf8193
Adam Avramov <fallenblood@gmail.com>
2016-07-23 16:46:23 +0300

    scaffold out terminal hook

--- plugins/Terminal/client/views/terminal.js
+++ plugins/Terminal/client/views/terminal.js
@@ -1,12 +1,32 @@
-module.exports = function () {
+var Terminal = require('terminal.js')
+
+module.exports = function (frame, index) {
 
   return h('.TerminalFrame',
     [ h('.Frame_Header',
       [ $.lib.icon('terminal.fa-2x')
       , h('strong', 'Terminal' )
-      , h('.Frame_Close', { onclick: close }, '×')
-      ])
-    , h('.Terminal')
-    ]);
+      , h('.Frame_Close', { onclick: close }, '×') ])
+    , h('.Terminal', { hookScroll: TerminalHook(frame.id) }) ]);
+
+}
+
+function TerminalHook (id) {
+
+  return require('virtual-hook')(
+    { hook: function (element) {
+        console.debug('attaching terminal, id', id);
+        setTimeout(function () {
+          if (!__.model()[id]) {
+            console.debug('opening new terminal, id', id);
+            __.model.put(id, new Terminal({ cols: 80, rows: 25 }))
+          } else {
+            console.debug('found terminal, id', id);
+          }
+        }, 0);
+      }
+    , unhook: function (element) {
+        console.debug('detaching terminal, id', id);
+      } })
 
 }

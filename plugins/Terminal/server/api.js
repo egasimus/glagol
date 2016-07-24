909ca46bfd58e5e5a04941d427a38e2f06615b22
Adam Avramov <fallenblood@gmail.com>
2016-07-24 18:21:38 +0300

    start fleshing out terminal api

--- plugins/Terminal/server/api.js
+++ plugins/Terminal/server/api.js
@@ -2,14 +2,42 @@ module.exports = function (state, respond) {
 
   return {
 
-    Create:
+    Spawn:
+      function () {}
+
+    Kill:
+      function () {}
+
+    Attach:
       function (id) {
+        id = id || $.lib.makeId();
+        var pty = _.model.Instances()[id];
+        if (!pty) _.model.Instances.put(id, pty = spawn());
+        if (!pty._glagolHandler) {
+          pty.on('data', pty._glagolHandler = function (data) {
+            // TODO update
+          })
+        }
       },
 
-    Destroy:
+    Detach:
       function (id) {
+        var pty = _.model.Instances()[id];
+        pty.off('data', pty._glagolHandler);
       }
 
   }
 
+}
+
+function spawn () {
+  return require('pty.js').spawn(
+    process.platform === 'win32' ? 'cmd.exe' : 'bash',
+    [],
+    { name: 'xterm-256color'
+    , cols: 80
+    , rows: 24
+    , cwd: process.env.PWD,
+    , env: process.env})
+  }
 }

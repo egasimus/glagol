35442b2a25f87494f535358111ea7cfe8cd5de1f
Adam Avramov <fallenblood@gmail.com>
2016-07-24 13:28:51 +0300

    start refactoring file type detection

--- plugins/FS/client/views/file.js
+++ plugins/FS/client/views/file.js
@@ -1,131 +1,129 @@
-(function (frame, index) {
+var setContentType
+  , render;
 
+module.exports = function (frame, index) {
+  return 'loading...!'
   var file = App.Model.FS.Files()[frame.address]
-
   if (!file) {
     App.API('FS/GetInfo', frame.address)
     return 'loading...'
   }
-
-  switch (file.contentType) {
-    case 'audio/mpeg':
-    case 'audio/x-wav':
-      renderAudio();
-      break;
-    case 'image/png':
-    case 'image/jpeg':
-    case 'image/gif':
-      renderImage();
-      break;
-    case 'text/plain':
-      renderPlaintext();
-      break;
-    default:
-      unknownType();
-  }
-
+  setContentType(file.contentType, App.Model.Workspace.Frames.get(index));
   return body;
+}
 
-  function renderPlaintext () {
-    var ext = require('path').extname(file.path);
-    switch (ext) {
-      case '.m3u8':
-        body = defaultLayout(
-          __.__.Sound.views.playlist(frame, file),
-          h('.File_Toolbar',
-            [ h('button', $.lib.icon('refresh'))
-            , h('button', $.lib.icon('save')),
-            , h('button', $.lib.icon('eye')) ]));
-        break;
-      default:
-        body = defaultLayout(_.textEditor(file.path));
-    }
-  }
-  
-  function renderAudio () {
-    var playerId = frame.id + '_' + file.path;
-    if (!App.Model.Sound.Players()[playerId])   loadAudioPlayer(file.path, playerId);
-    if (!App.Model.Sound.Metadata()[file.path]) loadAudioMetadata(file.path);
-    body = _.sound(frame.id, file.path);
-  }
+setContentType = require('riko-route')(
+  [ [ /audio\/.+/, 'sound'      ]
+  , [ /image\/.+/, 'image'      ]
+  , [ /text\/.+/,  'textEditor' ]
+  ], { catchall: render.unknown })
 
-  function renderImage () {
-    body = defaultLayout(h('div.ImageContainer', addSrc(h('img'))))
-  }
+render =
 
-  function unknownType () {
-    body = defaultLayout(h('.File_Unknown',
-      [ $.lib.icon('info-circle')
-      , h('em.File_Unknown_Title', file.path)
-      , h('br')
-      , 'has an unfamiliar type, '
-      , h('em', file.contentType)
-      , h('br')
-      , 'Open as: '
-      , h('button', { onclick: setType('textEditor')  }, 'Plaintext')
-      , ' '
-      , h('button', { onclick: setType('hexEditor')   }, 'Binary')
-      , ' '
-      , h('button', { onclick: setType('selecttype')  }, 'Other type...') ]), false);
-  }
+  { unknown:
+      function (input, frame) {
+        returndefaultLayout(h('.File_Unknown',
+        [ $.lib.icon('info-circle')
+        , h('em.File_Unknown_Title', file.path)
+        , h('br')
+        , 'has an unfamiliar type, '
+        , h('em', file.contentType)
+        , h('br')
+        , 'Open as: '
+        , h('button', { onclick: setType('textEditor')  }, 'Plaintext')
+        , ' '
+        , h('button', { onclick: setType('hexEditor')   }, 'Binary')
+        , ' '
+        , h('button', { onclick: setType('selecttype')  }, 'Other type...') ]), false); }
 
-  function setType (type) {
-    return function () {
-      App.API('Workspace/Change', frame.id, 'type', type);
-    }
-  }
-
-  function addSrc (vnode) {
-    vnode.properties.src = '/api/FS/ReadFile?' + JSON.stringify([file.path]);
-    return vnode;
-  }
-
-  function defaultLayout (body, toolbar) {
-    return [
-      toolbar || h('.File_Toolbar',
-        [ h('button', $.lib.icon('refresh')) ]),
-      h('.File_Body',
-        [ h('header.Frame_Header',
-           [ $.lib.icon('file.fa-2x')
-           , h('input.Frame_Address',
-             { onchange: changeAddress
-             , value:    file.path })
-           , h('.Frame_Close', { onclick: close }, '×')
-           ])
-        , body
-        ]) ];
-
-    function changeAddress (event) {
-      event.preventDefault();
-      App.API('Workspace/Change', frame.id, 'address', event.target.value);
-    }
-
-    function close (event) {
-      event.preventDefault();
-      App.API('Workspace/Close', frame.id);
-    }
-  }
-
-  function loadAudioPlayer (src, playerId) {
-    setTimeout(function () {
-      App.Model.Sound.Players.put(playerId, $.plugins.Sound.player(src))
-    }, 0);
-  }
-
-  function loadAudioMetadata (src) {
-    setTimeout(function () {
-      App.Model.Sound.Metadata.put(src, 'loading');
-      var url = '/api/Sound/GetMetadata?' + JSON.stringify([src])
-        , xhr = new XMLHttpRequest();
-      xhr.open('GET', url, true);
-      xhr.onload = function () {
-        App.Model.Sound.Metadata.put(src, JSON.parse(xhr.response).data);
+  , textEditor:
+      function () {
+        var ext = require('path').extname(file.path);
+        switch (ext) {
+          case '.m3u8':
+            body = defaultLayout(
+              __.__.Sound.views.playlist(frame, file),
+              h('.File_Toolbar',
+                [ h('button', $.lib.icon('refresh'))
+                , h('button', $.lib.icon('save')),
+                , h('button', $.lib.icon('eye')) ]));
+            break;
+          default:
+            body = defaultLayout(_.textEditor(file.path));
+        }
       }
-      xhr.onerror = function () {
-        App.Model.Sound.Metadata.put(src, 'failed');
-      }
-      xhr.send();
-    })
-  }
 
-})
+  , sound:  
+      function () {
+        var playerId = frame.id + '_' + file.path;
+        if (!App.Model.Sound.Players()[playerId])   loadAudioPlayer(file.path, playerId);
+        if (!App.Model.Sound.Metadata()[file.path]) loadAudioMetadata(file.path);
+        body = _.sound(frame.id, file.path);
+      }
+
+  , image:
+      function () {
+        body = defaultLayout(h('div.ImageContainer', addSrc(h('img'))))
+      }
+
+  , defaultLayout:
+      function (body, toolbar) {
+        return [
+          toolbar || h('.File_Toolbar',
+            [ h('button', $.lib.icon('refresh')) ]),
+          h('.File_Body',
+            [ h('header.Frame_Header',
+               [ $.lib.icon('file.fa-2x')
+               , h('input.Frame_Address',
+                 { onchange: changeAddress
+                 , value:    file.path })
+               , h('.Frame_Close', { onclick: close }, '×')
+               ])
+            , body
+            ]) ];
+
+        function changeAddress (event) {
+          event.preventDefault();
+          App.API('Workspace/Change', frame.id, 'address', event.target.value);
+        }
+
+        function close (event) {
+          event.preventDefault();
+          App.API('Workspace/Close', frame.id);
+        }
+      }
+
+  };
+
+function setType (type) {
+  return function () {
+    App.API('Workspace/Change', frame.id, 'type', type);
+  }
+}
+
+function addSrc (vnode) {
+  vnode.properties.src = '/api/FS/ReadFile?' + JSON.stringify([file.path]);
+  return vnode;
+}
+
+function loadAudioPlayer (src, playerId) {
+  setTimeout(function () {
+    App.Model.Sound.Players.put(playerId, $.plugins.Sound.player(src))
+  }, 0);
+}
+
+function loadAudioMetadata (src) {
+  setTimeout(function () {
+    App.Model.Sound.Metadata.put(src, 'loading');
+    var url = '/api/Sound/GetMetadata?' + JSON.stringify([src])
+      , xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+      App.Model.Sound.Metadata.put(src, JSON.parse(xhr.response).data);
+    }
+    xhr.onerror = function () {
+      App.Model.Sound.Metadata.put(src, 'failed');
+    }
+    xhr.send();
+  })
+}

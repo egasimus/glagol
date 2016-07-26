6f5210ae831b00f8c2f392ce43eb084465df89c8
Adam Avramov <fallenblood@gmail.com>
2016-07-26 19:47:38 +0300

    refactor mixer code

--- plugins/Sound/client/devices/mixer.js
+++ plugins/Sound/client/devices/mixer.js
@@ -1,14 +1,16 @@
-module.exports = function (id) {
+var extend = require('extend');
 
-  var context = __.model().context
-    , mixer =
-      { id:       id
-      , context:  context
-      , master:   context.createGain()
-      , meter:    _.meter(context) }
+module.exports = function (mixerId) {
+
+  var context = __.model().context;
+
+  var mixer =
+    { id:      mixerId
+    , context: context
+    , master:  context.createGain()
+    , meter:   _.meter(context) };
 
   mixer.channels = [0,1,2,3].map(mixerChannel)
-  mixer.channels.forEach(function (channel) { channel.fader.connect(mixer.master); })
   mixer.master.connect(context.destination);
   mixer.master.connect(mixer.meter);
 
@@ -16,36 +18,32 @@ module.exports = function (id) {
 
   function mixerChannel (i) {
 
-    var channelId = id + '/' + i
-      , channel =
-        { id:      channelId
+    var channel =
+        { id:      mixerId + '/' + i
         , gain:    context.createGain()
+        , low:     filter('lowshelf',  150)
+        , lowMid:  filter('peaking',   1000)
+        , highMid: filter('peaking',   4000)
+        , high:    filter('highshelf', 10000)
         , pan:     context.createStereoPanner()
-        , high:    context.createBiquadFilter()
-        , highMid: context.createBiquadFilter()
-        , lowMid:  context.createBiquadFilter()
-        , low:     context.createBiquadFilter()
         , fader:   context.createGain()
         , meter:   _.meter(context) };
 
     channel.gain.connect(channel.low);
-    channel.low.type.value          = 'lowshelf';
-    channel.low.frequency.value     = 150;
     channel.low.connect(channel.lowMid)
-    channel.lowMid.type.value       = 'peaking';
-    channel.lowMid.frequency.value  = 1000;
     channel.lowMid.connect(channel.highMid)
-    channel.highMid.type.value      = 'peaking';
-    channel.highMid.frequency.value = 3000;
     channel.highMid.connect(channel.high)
-    channel.high.type.value         = 'highshelf';
-    channel.high.frequency.value    = 10000
     channel.high.connect(channel.fader);
     channel.fader.connect(channel.meter);
     channel.fader.connect(mixer.master);
 
     return channel;
 
+  }
+
+  function filter (type, freq) {
+    return extend(true, context.createBiquadFilter(),
+      { type: type, frequency: { value: freq } })
   }
 
 }

d34408ecc029e7b151985ad38e5c67c584ffec78
Adam Avramov <fallenblood@gmail.com>
2016-07-26 21:35:30 +0300

    make knob a widget

--- plugins/Control/client/views/knob.js
+++ plugins/Control/client/views/knob.js
@@ -1,6 +1,40 @@
+var vdom = require('virtual-dom')
+
 module.exports = function knob (label, value) {
+
+  return require('vdom-thunk')(module.exports.widget, label, value);
+
+}
+
+module.exports.widget = function (label, value) {
+
+  var widget =
+    { type:
+        'Widget'
+    , init:
+        function () {
+          this.element = vdom.create(module.exports.render(label, value));
+          return this.element; }
+    , update:
+        function (prev, el) {
+          this.element = this.element || prev.element; }
+    , destroy:
+        function (el) {} }
+
+  return widget;
+
+}
+
+module.exports.render = function (label, value) {
+
   return h('.Knob_Label',
     [ h('.Knob_Label_Text',
       [ h('div', label || '')
       , h('div', String(value || '0')) ])
-    , h('.Knob') ]) }
+    , h('.Knob', { onclick: onclick }) ])
+
+}
+
+function onclick () {
+  console.log('click')
+}

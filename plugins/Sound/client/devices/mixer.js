c12920ffd0faf31d192a9d0bbd2a99eddac4a6c9
Adam Avramov <fallenblood@gmail.com>
2016-07-26 19:55:58 +0300

    extend doesn't work with audionodes

--- plugins/Sound/client/devices/mixer.js
+++ plugins/Sound/client/devices/mixer.js
@@ -13,22 +13,19 @@ module.exports = function (mixerId) {
   mixer.channels = [0,1,2,3].map(mixerChannel)
   mixer.master.connect(context.destination);
   mixer.master.connect(mixer.meter);
-
   return mixer;
 
   function mixerChannel (i) {
-
     var channel =
-        { id:      mixerId + '/' + i
-        , gain:    context.createGain()
-        , low:     filter('lowshelf',  150)
-        , lowMid:  filter('peaking',   1000)
-        , highMid: filter('peaking',   4000)
-        , high:    filter('highshelf', 10000)
-        , pan:     context.createStereoPanner()
-        , fader:   context.createGain()
-        , meter:   _.meter(context) };
-
+      { id:      mixerId + '/' + i
+      , gain:    context.createGain()
+      , low:     filter('lowshelf',  150)
+      , lowMid:  filter('peaking',   1000)
+      , highMid: filter('peaking',   4000)
+      , high:    filter('highshelf', 10000)
+      , pan:     context.createStereoPanner()
+      , fader:   context.createGain()
+      , meter:   _.meter(context) };
     channel.gain.connect(channel.low);
     channel.low.connect(channel.lowMid)
     channel.lowMid.connect(channel.highMid)
@@ -36,14 +33,14 @@ module.exports = function (mixerId) {
     channel.high.connect(channel.fader);
     channel.fader.connect(channel.meter);
     channel.fader.connect(mixer.master);
-
     return channel;
-
   }
 
   function filter (type, freq) {
-    return extend(true, context.createBiquadFilter(),
-      { type: type, frequency: { value: freq } })
+    var filter = context.createBiquadFilter();
+    filter.type = type;
+    filter.frequency.value = freq;
+    return filter;
   }
 
 }

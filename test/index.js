50f624d47e4538f1470321c2312ec4b5a90dbce8
Adam Avramov <fallenblood@gmail.com>
2016-07-28 00:25:05 +0300

    add basic tests for client-side loader

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var test = require('tape')
 
 test('options', function (t) {
 
-  t.plan(7)
+  t.plan(8);
 
   var f = glagol.File('test', { foo: 'bar' })
   t.deepEqual(f.options, { foo: 'bar' })
@@ -25,5 +25,34 @@ test('options', function (t) {
 
   d.add(f);
   t.deepEqual(f.options, { foo: 'bar', bar: 'baz', quux: 'asdf' })
+
+  d.options.quux = 'qwer';
+  t.equal(f.options.quux, 'qwer');
+
+})
+
+test('browser', function (t) {
+
+  var browser =
+    { loader:  require('../browser/loader')
+    , require: require('../browser/require')
+    , vm:      require('../browser/vm') };
+
+  var fixture =
+    { 'A.js':  '(function () {})'
+    , 'B.txt': '8'
+    , 'C.d': { 'D.json': '{"foo":"bar..."}' } };
+
+  t.plan(6);
+
+  var load = browser.loader()
+    , root = load(fixture);
+
+  t.equal(root.get('A.js').source,       fixture['A.js']);
+  t.equal(root.get('B.txt').source,      fixture['B.txt']);
+  t.equal(root.get('C.d/D.json').source, fixture['C.d']['D.json']);
+  t.equal(typeof root.get('A.js').value,       'function');
+  t.equal(typeof root.get('B.txt').value,      'string');
+  t.equal(typeof root.get('C.d/D.json').value, 'object');
 
 })

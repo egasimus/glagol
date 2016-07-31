feae2a703258ddb1c1037605f45fbdd4a0d3a88c
Adam Avramov <fallenblood@gmail.com>
2016-07-31 23:19:58 +0300

    json inherits from plaintext

--- formats/json.js
+++ formats/json.js
@@ -1,10 +1,6 @@
 module.exports =
-  { compile: function compile (file) {
-      return file.source.replace(/\n$/, ""); // strip trailing newline
-    }
-  , evaluate: function evaluate (file) {
-      return JSON.parse(file.compiled);
-    }
-  , globals: function globals (file) {}
-  , name:   "JSON"
-  , target: "plaintext" };
+  { compile:  require('./plaintext').compile
+  , evaluate: function evaluate (file) { return JSON.parse(file.compiled); }
+  , globals:  function globals (file) {}
+  , name:     "JSON"
+  , target:   "plaintext" };

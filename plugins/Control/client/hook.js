51a50c9ef6e080ff120da8a59bab06e14a9e300b
Adam Avramov <fallenblood@gmail.com>
2016-08-01 00:05:15 +0300

    mixer knobs can now be twiddled

--- plugins/Control/client/hook.js
+++ plugins/Control/client/hook.js
@@ -1,23 +1,34 @@
 module.exports = function DragHook (dragDirection) {
 
-  var initial = null;
+  var element  = null
+    , knob     = null
+    , initialY = null
+    , minValue = -135
+    , maxValue = 135
+    , value    = 0
 
   return require('virtual-hook')({ hook: hook, unhook: unhook });
   
   function hook (el) {
-    el.addEventListener('mousedown', start); }
+    setTimeout(function () {
+      element = el;
+      knob = el.querySelector('.Knob');
+      el.addEventListener('mousedown', start); }, 0) }
 
   function unhook (el) {
     el.removeEventListener('mousedown', start); }
 
   function start (event) {
-    initial = event.y;
+    initialY = event.y;
     window.addEventListener('mousemove',  move);
     window.addEventListener('mouseup',    stop);
     window.addEventListener('mouseenter', stop); }
 
   function move (event) {
-    console.log(initial - event.y); }
+    var difference = initialY - event.y;
+    initialY = event.y;
+    value = Math.max(minValue, Math.min(maxValue, value + difference));
+    knob.style.transform = 'rotate(' + value + 'deg)' }
 
   function stop (event) {
     window.removeEventListener('mousemove',  move);

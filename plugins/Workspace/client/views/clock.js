9baf6a581d7e95379782769a1240d4cb15bc83a9
Adam Avramov <fallenblood@gmail.com>
2016-08-13 20:55:01 +0300

    make clock work

--- plugins/Workspace/client/views/clock.js
+++ plugins/Workspace/client/views/clock.js
@@ -1,6 +1,40 @@
 module.exports = function () {
- var now  = new Date()
-   , date = now.getFullYear() + '-' + now.getMonth()   + '-' + now.getDate()
-   , time = now.getHours()    + ':' + now.getMinutes() + ':' + now.getSeconds();
- return h('button.Taskbar_Clock', [ date, h('br'), time ])
+  return h('button.Taskbar_Clock', { hook: ClockHook() });
+}
+
+function ClockHook () {
+
+  var frame = null;
+
+  return require('virtual-hook')({ hook: hook, unhook: unhook });
+
+  function hook (element) {
+    frame = requestAnimationFrame(function render () {
+      element.innerHTML = getClockValue();
+      frame = requestAnimationFrame(render);
+    })
+  }
+
+  function unhook (element) {
+    cancelAnimationFrame(frame);
+  }
+
+}
+
+function getClockValue () {
+  var now = new Date()
+  return now.getYear()       +
+    '-' + pad(now.getMonth()) +
+    '-' + pad(now.getDate())  +
+    '<br>' + pad(now.getHours()) +
+    ':' + pad(now.getMinutes()) +
+    ':' + pad(now.getSeconds()) +
+    '.' + pad(now.getMilliseconds(), 3)
+}
+
+function pad (value, n) {
+  var length  = Math.max((n || 1) - String(value).length, 0)
+    , padding = '';
+  while (length--) padding += '0';
+  return padding + value;
 }

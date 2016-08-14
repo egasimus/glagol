5717f38522cb48258f7abcce6a4390dd70e96362
Adam Avramov <fallenblood@gmail.com>
2016-08-14 05:22:30 +0300

    calculate drag offsets

--- plugins/Workspace/client/views/frame.js
+++ plugins/Workspace/client/views/frame.js
@@ -6,15 +6,35 @@ module.exports = function (frame, index) {
 
   return h('section' + cls, options,
     [ content
-    , ['Top', 'Bottom', 'Left', 'Right'].map(function (direction) {
+    , ['Top', 'Bottom', 'Left', 'Right'].map(function (edge) {
         return h(
-          '.Frame_Dragger_' + direction,
+          '.Frame_Dragger_' + edge,
           { draggable: true
-          , ondrag:    resize(direction) } ); }) ]);
+          , ondrag:    resize(edge) } ); }) ]);
 
-  function resize (direction) {
+  function resize (edge) {
+    var previous = null;
     return function (event) {
-      console.log(direction, event);
+      var offset =
+            0
+        , axis =
+            (edge === 'Left' || edge === 'Right') ? 'X' :
+            (edge === 'Top' || edge === 'Bottom') ? 'Y' : undefined
+        , value =
+            event['offset' + axis]
+        , reverse =
+            edge === 'Top' || edge === 'Left'
+      if (previous) {
+        offset = value - previous;
+        previous = value;
+      } else {
+        previous = value;
+        offset =
+          value > 0 ?  1 :
+          value < 0 ? -1 : 0;
+      }
+      console.log(edge, offset);
+      console.log(__.model.Frames.get(index)())
     }
   }
 
